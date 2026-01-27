860e88d424b8d6d70d92fcea1a5aa35d4a8a1041
ridwancodexr <ridwancodexr@gmail.com>
2026-01-27 20:51:26 +0600

    all update

--- Lesson/Lesson-1/src/App.jsx
+++ Lesson/Lesson-1/src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import Home from "./Home";
 import Blogs from "./Blogs";
 import Products from "./Products";
+import Name from "./Name";
 function App() {
   return (
     <>
+      <Name></Name>
       <Home></Home>
       <Products></Products>
       <Blogs></Blogs>
---
+++ Lesson/Lesson-1/src/Name.jsx
@@ -0,0 +1,9 @@
+const Name = () => {
+  return (
+    <div>
+      <h1>This is Ridwan</h1>
+    </div>
+  );
+};
+
+export default Name;
