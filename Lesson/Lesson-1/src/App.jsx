68da1dd84a024b6338297d00bcc357af56044b53
ridwancodexr <ridwancodexr@gmail.com>
2026-01-27 20:05:08 +0600

    Products Component Create

--- Lesson/Lesson-1/src/App.jsx
+++ Lesson/Lesson-1/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Home from "./Home";
 import Blogs from "./Blogs";
-
+import Products from "./Products";
 function App() {
   return (
     <>
       <Home></Home>
+      <Products></Products>
       <Blogs></Blogs>
       <h1>Hello World </h1>
     </>
---
+++ Lesson/Lesson-1/src/Products.jsx
@@ -0,0 +1,15 @@
+function Products() {
+  return (
+    <div>
+      <ul>
+        <li>Laptop</li>
+        <li>Monitor</li>
+        <li>Mouse</li>
+        <li>Keybord</li>
+        <li>Light</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
