4bd5769583fcdd1aa65074935dd18462590eb92f
ridwancodexr <ridwancodexr@gmail.com>
2026-01-28 20:39:24 +0600

    Some file delete and some update

--- Lesson/Lesson-1/src/App.jsx
+++ Lesson/Lesson-1/src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import Home from "./Home";
-import Products from "./Products";
-import Name from "./Name";
-import Blogs from "./Blogs";
-import About from "./About";
 function App() {
   return (
     <>
-      <Name></Name>
-      <Home></Home>
-      <Blogs></Blogs>
-      <Products></Products>
-      <About></About>
       <h1>Hello World </h1>
     </>
   );
--- Lesson/Lesson-1/src/Name.jsx
+++
@@ -1,9 +0,0 @@
-const Name = () => {
-  return (
-    <div>
-      <h1>This is Ridwan</h1>
-    </div>
-  );
-};
-
-export default Name;
