3b07a9f5ef957bc54311aab3d422851bc5e5834b
ridwancodexr <ridwancodexr@gmail.com>
2026-01-29 19:11:55 +0600

    Blog Component Create

--- Lesson/Lesson-1/src/App.jsx
+++ Lesson/Lesson-1/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Blog from "./components/Blog/Blog";
 import Home from "./components/Home/Home";
 function App() {
   return (
     <>
       <Home></Home>
+      <Blog></Blog>
       <h1>Hello World </h1>
     </>
   );
---
+++ Lesson/Lesson-1/src/components/Blog/Blog.jsx
@@ -0,0 +1,19 @@
+import "./Blog.css";
+
+const Blog = () => {
+  return (
+    <div>
+      <p className="firstPara">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, repellat!
+      </p>
+      <br />
+      <p>
+        <b>Lorem ipsum dolor sit amet.</b> Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Blanditiis, esse incidunt corrupti
+        necessitatibus nulla tempore!{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Blog;
