a95cdc0b8590e5ff03390c2346eed87d2f2b40e3
ridwancodexr <ridwancodexr@gmail.com>
2026-01-29 17:23:28 +0600

    components File create

--- Lesson/Lesson-1/src/App.jsx
+++ Lesson/Lesson-1/src/App.jsx
@@ -1,12 +1,8 @@
-import About from "./About";
 import "./App.css";
-import Contact from "./Contact";
 function App() {
   return (
     <>
       <h1>Hello World </h1>
-      <Contact></Contact>
-      <About></About>
     </>
   );
 }
--- Lesson/Lesson-1/src/Contact.jsx
+++
@@ -1,9 +0,0 @@
-function Contact() {
-  return (
-    <div>
-      <h2>Hello , This is a Contact Section</h2>
-    </div>
-  );
-}
-
-export default Contact;
