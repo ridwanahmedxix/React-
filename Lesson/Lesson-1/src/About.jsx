d1fdf5e8c36343ce6c7fd18c7a77e8565f264839
ridwancodexr <ridwancodexr@gmail.com>
2026-01-29 17:00:34 +0600

    toggle system create

--- Lesson/Lesson-1/src/About.jsx
+++ Lesson/Lesson-1/src/About.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h2>Welcome To The About Page </h2>
+      <br />
+      <p>
+        <i>Lorem ipsum dolor sit amet consectetur adipisicing elit.</i>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tempora
+        perspiciatis aliquam ad doloribus soluta. Aspernatur nam natus quos
+        quam.{" "}
+        <b>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic.</b>
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- Lesson/Lesson-1/src/App.jsx
+++ Lesson/Lesson-1/src/App.jsx
@@ -1,8 +1,10 @@
+import About from "./About";
 import "./App.css";
 function App() {
   return (
     <>
       <h1>Hello World </h1>
+      <About></About>
     </>
   );
 }
