5d0b87ec286f2ca33e9e28da7ee7374db8bfa7fb
ridwancodexr <ridwancodexr@gmail.com>
2026-01-30 09:36:30 +0600

    Products Component create

---
+++ Lesson/Lesson-1/src/components/Products/Products.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
