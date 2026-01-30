8562a70dee10af41dffef7d28477f8bd1dfb8704
ridwancodexr <ridwancodexr@gmail.com>
2026-01-30 11:11:14 +0600

    Click to Inner Text Change

--- Lesson/Lesson-1/src/components/Products/Products.jsx
+++ Lesson/Lesson-1/src/components/Products/Products.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import "./Products.css";
 
 const Products = () => {
+  function btnClick() {
+    const paraTag = document.querySelector(".paraTag");
+    paraTag.innerText = " Hello I am next Compo";
+  }
+
   return (
     <div>
       <p className="paraTag">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
         molestias.
       </p>
-      <button className="clickBtn">Click Me</button>
+      <button onClick={btnClick} className="clickBtn">
+        Click Me
+      </button>
     </div>
   );
 };
