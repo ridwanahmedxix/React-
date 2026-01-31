7d9385aa5b4ab0fe60e49618769ae3250d9f1a37
ridwancodexr <ridwancodexr@gmail.com>
2026-01-31 09:39:33 +0600

    App & DyData File some update code

--- Lesson/Lesson-1/src/App.jsx
+++ Lesson/Lesson-1/src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import Maths from "./components/Maths/Maths";
 import Products from "./components/Products/Products";
 function App() {
   return (
     <>
-      <Maths></Maths>
       <Products></Products>
     </>
   );
--- Lesson/Lesson-1/src/components/DyData/DyData.jsx
+++ Lesson/Lesson-1/src/components/DyData/DyData.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./DyData.css";
 
 const DyData = () => {
   return <div>DyData</div>;
