1216c5d7f5a6916945da70583fad4df39f74b488
ridwancodexr <ridwancodexr@gmail.com>
2026-01-31 18:16:01 +0600

    Name Age Address list add

---
+++ Practice/practice1/src/components/Peoples/Peoples.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Peoples.css";
+
+const Peoples = () => {
+  return (
+    <div>
+      <h3>Name :</h3>
+      <h3>Age :</h3>
+      <h3>Address : </h3>
+    </div>
+  );
+};
+
+export default Peoples;
