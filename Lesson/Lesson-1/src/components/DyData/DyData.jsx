d0f6246cc6e3a921202d7b1bdfa3fb309fae2b40
ridwancodexr <ridwancodexr@gmail.com>
2026-01-31 13:59:53 +0600

    Props Name Check

--- Lesson/Lesson-1/src/components/DyData/DyData.jsx
+++ Lesson/Lesson-1/src/components/DyData/DyData.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./DyData.css";
 
 const DyData = (props) => {
-  console.log(props);
+  console.log(props.name);
   return (
     <div id="ExploreDyData">
       <h2>Explore Daynamic Data </h2>
