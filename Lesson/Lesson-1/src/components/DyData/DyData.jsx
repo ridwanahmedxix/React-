fda8776abe3e67191bf62c8b8828647f3f0095a9
ridwancodexr <ridwancodexr@gmail.com>
2026-01-31 14:06:32 +0600

    Props To Name Show

--- Lesson/Lesson-1/src/components/DyData/DyData.jsx
+++ Lesson/Lesson-1/src/components/DyData/DyData.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./DyData.css";
 
 const DyData = (props) => {
-  console.log(props.name);
+  // console.log(props.name);
   return (
     <div id="ExploreDyData">
       <h2>Explore Daynamic Data </h2>
+      <h2> Name: {props.name} </h2>
     </div>
   );
 };
