26b75a2f32a47eaede3ced7090d2d54ea5e03f0a
ridwancodexr <ridwancodexr@gmail.com>
2026-02-01 09:22:41 +0600

    HeroArea Html structure Create

---
+++ Lesson/Lesson-1/src/components/HeroArea/HeroArea.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./HeroArea.css";
+const HeroArea = () => {
+  return (
+    <div>
+      <div id="div1">
+        <h1>Model Collage </h1>
+        <p>Ridwan Ahmed </p>
+      </div>
+      <div id="div2">
+        <p>
+          <b>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</b> Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Vitae sequi modi
+          voluptatum. Officiis deserunt sunt, beatae unde veniam at vel.{" "}
+        </p>
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>Products</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default HeroArea;
