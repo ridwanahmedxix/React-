908ec0dcf6b42b86fdc19706f9eaeda14ea1ca33
ridwancodexr <ridwancodexr@gmail.com>
2026-02-02 18:33:12 +0600

    First Commit

--- Lesson/Lesson-2/src/components/Home/Home.jsx
+++ Lesson/Lesson-2/src/components/Home/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Home.css";
 
-const Home = (props) => {
+const Home = () => {
   return (
     <div>
-      <h1>
-        Home Name : {props.homes.name} & Home Price {props.homes.price} :{" "}
-      </h1>
+      <h1>Products Name : </h1>
+      <h2> Products Price : </h2>
     </div>
   );
 };
