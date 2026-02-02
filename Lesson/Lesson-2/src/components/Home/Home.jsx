4b52aa1fede31c53c243fead2b41ed42f0bed616
ridwancodexr <ridwancodexr@gmail.com>
2026-02-02 20:56:09 +0600

    some update

--- Lesson/Lesson-2/src/components/Home/Home.jsx
+++ Lesson/Lesson-2/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 const Home = (props) => {
   return (
     <div>
-      <h1> Peoples Name : {props.peopleName[2]} </h1>
+      <h1 id="pName"> Peoples Name : {props.peopleName[2]} </h1>
       <h2> Peoples Age : {props.peopleAge} </h2>
     </div>
   );
