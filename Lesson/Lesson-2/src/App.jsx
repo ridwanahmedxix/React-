0323717a3b1d7f20d0363c1ef7dec1cb12fb3268
ridwancodexr <ridwancodexr@gmail.com>
2026-02-02 18:49:57 +0600

    2 array create

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Home from "./components/Home/Home";
 
 function App() {
+  const peopleName = ["Ridwan", "Aideed", "Naim", "Talha", "Shamim"];
+  const peopleAge = [18, 19, 20, 21, 22];
   return (
     <div>
       <Home></Home>
