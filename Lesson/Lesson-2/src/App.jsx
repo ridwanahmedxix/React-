bb97e339d3d08505f3a7cd62eea101eb2a1c3888
ridwancodexr <ridwancodexr@gmail.com>
2026-02-03 10:33:19 +0600

    all clear

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -2,30 +2,9 @@ import "./App.css";
 import Home from "./components/Home/Home";
 
 function App() {
-  const products = [
-    {
-      name: "Laptop",
-      price: 1200,
-    },
-    {
-      name: "Mobile",
-      price: 800,
-    },
-    {
-      name: "Mouse",
-      price: 150,
-    },
-  ];
-
-  const cfAlert = () => {
-    alert(" This is a Alert");
-  };
-
   return (
     <div>
-      {products.map((product) => (
-        <Home product={product} clickAlert={cfAlert}></Home>
-      ))}
+      <h1>Hello</h1>
     </div>
   );
 }
