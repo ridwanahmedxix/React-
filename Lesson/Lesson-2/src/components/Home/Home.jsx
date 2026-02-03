84250d2a9fe8989e0ca1ecdc6e878330857f93ca
ridwancodexr <ridwancodexr@gmail.com>
2026-02-03 09:42:57 +0600

    Alert Create

--- Lesson/Lesson-2/src/components/Home/Home.jsx
+++ Lesson/Lesson-2/src/components/Home/Home.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import "./Home.css";
 
 const Home = (props) => {
+  const cfAlert = document.getElementById("cfAlert");
+  cfAlert.addEventListener("click", () => {
+    alert(" This is a Alert");
+  });
+
   return (
     <div>
       <h1> Product Name : {props.product.name} </h1>
       <h2> Product Price : {props.product.price} </h2>
+      <button id="cfAlert">Click For Alert</button>
     </div>
   );
 };
