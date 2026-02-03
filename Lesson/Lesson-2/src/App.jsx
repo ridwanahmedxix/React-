d226ef396781315dc916c336f6d175866f79ce77
ridwancodexr <ridwancodexr@gmail.com>
2026-02-03 10:58:52 +0600

    Increase and Decrease Button Create

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./App.css";
-import Home from "./components/Home/Home";
 
 function App() {
+  let [price, setPrice] = useState(10);
+
+  const increaseHandle = () => {
+    const NewPrice = price + 1;
+    setPrice(NewPrice);
+  };
+
+  const decreaseHandle = () => {
+    setPrice(price - 1);
+  };
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1> Price: {price} </h1>
+      <button onClick={increaseHandle}>Increase</button>
+      <button onClick={decreaseHandle}>Decrease</button>
     </div>
   );
 }
