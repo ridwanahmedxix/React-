6f790e340670661ea2980b0d30be6506e7d88e6f
ridwancodexr <ridwancodexr@gmail.com>
2026-02-03 14:24:48 +0600

    decrease button create

--- Practice/project2/src/App.jsx
+++ Practice/project2/src/App.jsx
@@ -7,10 +7,17 @@ const App = () => {
     const newPrice = price + 1;
     setPrice(newPrice);
   };
+
+  const decreaseHandele = () => {
+    const newPrice = price - 1;
+    setPrice(newPrice);
+  };
+
   return (
     <div>
       <h1> Price : {price} </h1>
       <button onClick={increaseHandle}>Increase</button>
+      <button onClick={decreaseHandele}>Decrease</button>
     </div>
   );
 };
