877a80c121e6e54e6ac044c6a7f5ecce48d866ea
ridwancodexr <ridwancodexr@gmail.com>
2026-02-03 09:24:44 +0600

    One Array and 3 Objects Create

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -2,6 +2,21 @@ import "./App.css";
 import Home from "./components/Home/Home";
 
 function App() {
+  const products = [
+    {
+      name: "Laptop",
+      price: 1200,
+    },
+    {
+      name: "Mobile",
+      price: 800,
+    },
+    {
+      name: "Mouse",
+      price: 150,
+    },
+  ];
+
   return <div></div>;
 }
 
