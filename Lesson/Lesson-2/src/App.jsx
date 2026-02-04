cc73d070574ea5c3cf9d3e2eb0550d107e63bcd8
ridwancodexr <ridwancodexr@gmail.com>
2026-02-04 10:51:41 +0600

    useEffect Import

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseNumber = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+
+  useEffect(() => {
+    alert("This is a Alert");
+  }, []);
+
   return (
     <div>
-      <h1>Home Page </h1>
+      <h1> Increase count : {count} </h1>
+      <button onClick={increaseNumber}>Click</button>
     </div>
   );
 }
