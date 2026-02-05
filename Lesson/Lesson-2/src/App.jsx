b08c329def1586335a55d731d95972487e3c8c5a
ridwancodexr <ridwancodexr@gmail.com>
2026-02-05 21:13:01 +0600

    button create

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Hello World </h1>
+      <h1> Count : {price} </h1>
+      <button onClick={increaseHandle}> Increase </button>
     </div>
   );
 }
