339693d703903e314bc3ce858ff1294d67325fb3
ridwancodexr <ridwancodexr@gmail.com>
2026-02-05 18:31:44 +0600

    App css file import

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -1,28 +1,2 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  const increaseNumber = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-  };
-
-  useEffect(() => {
-    alert("This is a Alert");
-  }, []);
-
-  useEffect(() => {
-    alert(" Count State Was Chabged");
-  }, [count]);
-
-  return (
-    <div>
-      <h1> Increase count : {count} </h1>
-      <button onClick={increaseNumber}>Click</button>
-    </div>
-  );
-}
-
-export default App;
