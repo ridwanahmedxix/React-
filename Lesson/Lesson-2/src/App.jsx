f84f6a9fa57e592723eb3f338de2f7af7d150c61
ridwancodexr <ridwancodexr@gmail.com>
2026-02-06 19:28:10 +0600

    Increase Button , useEffect , useState revison

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [price, setPrice] = useState(10);
+  const [price, setPrice] = useState(20);
+
   const increaseButton = () => {
     const newPrice = price + 1;
     setPrice(newPrice);
   };
 
   useEffect(() => {
-    alert("This is a Use Alert");
+    alert("This is a First Use Effect");
   }, []);
+
+  useEffect(() => {
+    alert("Price State Was Changed ");
+  }, [price]);
 
   return (
     <div>
       <h1> Count : {price} </h1>
-      <button onClick={increaseButton}>Increase </button>
+      <button onClick={increaseButton}>Increase</button>
     </div>
   );
 }
