8c5bdf102a069cab4666638ce306f03b7484c614
ridwancodexr <ridwancodexr@gmail.com>
2026-02-06 19:07:23 +0600

    Use Effect Alert Set

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -1,26 +1,14 @@
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increaseNumber = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-  };
-
   useEffect(() => {
-    alert("This is a Alert");
+    alert("This is a Use Alert");
   }, []);
-
-  useEffect(() => {
-    alert(" Count State Was Chabged");
-  }, [count]);
 
   return (
     <div>
-      <h1> Increase count : {count} </h1>
-      <button onClick={increaseNumber}>Click</button>
+      <h1>Hello World </h1>
     </div>
   );
 }
