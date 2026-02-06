7e1756cda1777c43567500c7169e237a51878b85
ridwancodexr <ridwancodexr@gmail.com>
2026-02-06 19:10:56 +0600

    Increase Button add

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -1,14 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [price, setPrice] = useState(10);
+  const increaseButton = () => {
+    const newPrice = price + 1;
+    setPrice(newPrice);
+  };
+
   useEffect(() => {
     alert("This is a Use Alert");
   }, []);
 
   return (
     <div>
-      <h1>Hello World </h1>
+      <h1> Count : {price} </h1>
+      <button onClick={increaseButton}>Increase </button>
     </div>
   );
 }
