72e88192846bcde1d3c88750320dd148f787099f
ridwancodexr <ridwancodexr@gmail.com>
2026-02-07 19:36:33 +0600

    fetching Data in React with useEffect |

--- Lesson/Lesson-3/src/components/SingleuserData/SingleuserData.jsx
+++ Lesson/Lesson-3/src/components/SingleuserData/SingleuserData.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const SingleuserData = ({ singleData }) => {
-  return <div> {singleData.name} </div>;
+  const { id, name, phone, website } = singleData;
+
+  return (
+    <div>
+      <h1> Your id: {id} </h1>
+      <h2> Your name : {name} </h2>
+      <h3> Your phone : {phone} </h3>
+      <h4> Your website : {website} </h4>
+    </div>
+  );
 };
 
 export default SingleuserData;
