b761ebc86006027c85e9253d289eba07c23e008c
ridwancodexr <ridwancodexr@gmail.com>
2026-02-07 09:28:08 +0600

    Usestate - button to increase count

--- Lesson/Lesson-2/src/App.jsx
+++ Lesson/Lesson-2/src/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import "./App.css ";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [price, setPrice] = useState(10);
+
+  const increaseButton = () => {
+    const newPrice = price + 1;
+    setPrice(newPrice);
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1> Count : {price} </h1>
+      <button onClick={increaseButton}>Increase</button>
     </div>
   );
 }
