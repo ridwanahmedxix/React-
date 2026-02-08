1c2bae388739609aeb668777606c451b7c7456d7
ridwancodexr <ridwancodexr@gmail.com>
2026-02-08 11:19:25 +0600

    React Class 12 done

---
+++ Lesson/Lesson-3/src/components/SingleuserData/SingleuserData.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SingleuserData = ({ singleData }) => {
+  const { id, name, phone, website } = singleData;
+
+  return (
+    <div>
+      <h1> {id} </h1>
+      <h1> {name} </h1>
+      <h1> {phone} </h1>
+      <h1> {website} </h1>
+    </div>
+  );
+};
+
+export default SingleuserData;
