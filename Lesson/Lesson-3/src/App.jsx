cd8ff08553b834acb1478b1cee139da57f4dbb12
ridwancodexr <ridwancodexr@gmail.com>
2026-02-08 16:13:47 +0600

    useState and useEffect add

---
+++ Lesson/Lesson-3/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [allData, setAllData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setAllData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello World </h1>
+    </div>
+  );
+}
+
+export default App;
