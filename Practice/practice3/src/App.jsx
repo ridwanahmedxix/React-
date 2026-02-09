a62ac5d759557feea1e3ec872a65048a9b900eef
ridwancodexr <ridwancodexr@gmail.com>
2026-02-09 09:23:26 +0600

    App.jsx file setup

--- Practice/practice3/src/App.jsx
+++ Practice/practice3/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import singleUserData from "./components/singleUserData/singleUserData";
 function App() {
   const [allData, setAllData] = useState([]);
 
@@ -11,6 +11,10 @@ function App() {
   }, []).return(
     <div>
       <h1>Hello World</h1>
+
+      {allData.map((singleData) => (
+        <singleUserData singleData={singleData}></singleUserData>
+      ))}
     </div>,
   );
 }
---
+++ Practice/practice3/src/components/singleUserData/singleUserData.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const singleUserData = () => {
+  return <div>Hello</div>;
+};
+
+export default singleUserData;
