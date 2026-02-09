d4326bccf8d5cc4ca67308c85ba2b3caca2d4d54
ridwancodexr <ridwancodexr@gmail.com>
2026-02-09 10:35:03 +0600

    all update

--- Practice/practice3/src/App.jsx
+++ Practice/practice3/src/App.jsx
@@ -13,8 +13,6 @@ function App() {
 
   return (
     <div>
-      <h1>Hello </h1>
-
       {allData.map((singleData) => (
         <SingleUserData singleData={singleData}></SingleUserData>
       ))}
--- Practice/practice3/src/Components/SingleUserData.jsx
+++ Practice/practice3/src/Components/SingleUserData.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SingleUserData = ({ singleData }) => {
-  const { id, name, phone, website } = singleData;
+  const { id, name, phone, website, company } = singleData;
 
   return (
     <div>
@@ -9,6 +9,7 @@ const SingleUserData = ({ singleData }) => {
       <h1> Name : {name} </h1>
       <h1> Phone : {phone} </h1>
       <h1> Website : {website} </h1>
+      <h1> Company : {company.name} </h1>
     </div>
   );
 };
