6b404e125c89dc6b8b730bfe6a3ff9c5c27a4621
ridwancodexr <ridwancodexr@gmail.com>
2026-02-10 20:11:59 +0600

    useEffect link fetch

--- Practice/practice3/src/App.jsx
+++ Practice/practice3/src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [allDtta, setAllData] = useState([]);
-
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users");
+  }, []);
   return (
     <div>
       <h1>Hello World</h1>
