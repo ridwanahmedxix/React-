95005393de4a66e7d9262c441ceeabee90bc053a
ridwancodexr <ridwancodexr@gmail.com>
2026-02-10 20:27:15 +0600

    id , name , website , phone , company shoe

--- Practice/practice3/src/Components/SingleUserData.jsx
+++ Practice/practice3/src/Components/SingleUserData.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const SingleUserData = () => {
+const SingleUserData = ({ singleData }) => {
+  const { id, name, website, phone, company } = singleData;
+
   return (
     <div>
-      <h1>Id</h1>
+      <h1> Id : {id} </h1>
+      <h1> Name : {name} </h1>
+      <h1> Website : {website} </h1>
+      <h1> Phone : {phone} </h1>
+      <h1> Company : {company.name} </h1>
     </div>
   );
 };
