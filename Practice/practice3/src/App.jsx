1887cd88b5af010196e0410814458f055b961a7b
ridwancodexr <ridwancodexr@gmail.com>
2026-02-10 20:17:19 +0600

    map loop create

--- Practice/practice3/src/App.jsx
+++ Practice/practice3/src/App.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import SingleUserData from "./Components/singleUserData";
 
 function App() {
   const [allDtta, setAllData] = useState([]);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setAllData(data));
   }, []);
   return (
     <div>
       <h1>Hello World</h1>
+
+      {allDtta.map((singleData) => (
+        <SingleUserData singleData={singleData}></SingleUserData>
+      ))}
     </div>
   );
 }
---
+++ Practice/practice3/src/Components/SingleUserData.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleUserData = () => {
+  return (
+    <div>
+      <h1>Id</h1>
+    </div>
+  );
+};
+
+export default SingleUserData;
