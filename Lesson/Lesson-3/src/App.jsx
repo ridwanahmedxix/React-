32125e35d55532d89120b3424ed1692e8e6b77f9
ridwancodexr <ridwancodexr@gmail.com>
2026-02-11 08:55:12 +0600

    display grid

--- Lesson/Lesson-3/src/App.jsx
+++ Lesson/Lesson-3/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className="user-container">
       {allData.map((singleData) => (
         <SingleuserData singleData={singleData}></SingleuserData>
       ))}
