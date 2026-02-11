0dcdf46c9e4283eb762f8a4a300f43d08fe61ba4
ridwancodexr <ridwancodexr@gmail.com>
2026-02-11 10:19:38 +0600

    useState and useEffect import

--- Lesson/Lesson-4/src/App.jsx
+++ Lesson/Lesson-4/src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [allData, setAllData] = useState([]);
+  useEffect(() => {
+    fetch("userData.json")
+      .then((res) => res.json())
+      .then((data) => setAllData(data));
+  }, []);
+
   return (
     <div>
       <h1>Hello World </h1>
