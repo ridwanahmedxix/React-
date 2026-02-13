8f5a7e3ead12bd7dc14dd9b1ae334a12c5304454
ridwancodexr <ridwancodexr@gmail.com>
2026-02-13 19:27:15 +0600

    2 card flex

--- Practice/practice1/src/App.jsx
+++ Practice/practice1/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Products from "./Components/Products/Products";
+import Placement from "./Components/Placement/Placement";
 
 function App() {
   return (
-    <div>
+    <div className="App_JSX_Parent">
       <Products></Products>
+      <Placement></Placement>
     </div>
   );
 }
