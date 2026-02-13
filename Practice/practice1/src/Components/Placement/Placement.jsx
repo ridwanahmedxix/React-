ea9f3479057106e13333666a89f08f68b65a54b5
ridwancodexr <ridwancodexr@gmail.com>
2026-02-13 10:47:02 +0600

    Placement Component Create

---
+++ Practice/practice1/src/Components/Placement/Placement.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Placement.css";
+
+const Placement = () => {
+  return (
+    <div>
+      <h1>This is a Placement Sec</h1>
+    </div>
+  );
+};
+
+export default Placement;
