cea2383294f164fd4ccdbbaec1985b302b789b39
ridwancodexr <ridwancodexr@gmail.com>
2026-02-14 10:09:48 +0600

    UseState import

--- Practice/practice1/src/App.jsx
+++ Practice/practice1/src/App.jsx
@@ -4,6 +4,8 @@ import Products from "./Components/Products/Products";
 import Placement from "./Components/Placement/Placement";
 
 function App() {
+  const [place, setPlace] = useState([]);
+
   const AddToCard = (placement) => {
     console.log("Clicked", placement);
   };
