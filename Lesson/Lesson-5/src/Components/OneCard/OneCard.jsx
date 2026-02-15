8458ae8bc66351cdc2a1b5c56c74d031695d6e67
ridwancodexr <ridwancodexr@gmail.com>
2026-02-15 10:05:14 +0600

    OneCard Section Design

--- Lesson/Lesson-5/src/Components/OneCard/OneCard.jsx
+++ Lesson/Lesson-5/src/Components/OneCard/OneCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./OneCard.css";
 
 const OneCard = ({ singleCardInfo }) => {
   const { id, name, price, brand, color } = singleCardInfo;
 
   return (
-    <div>
+    <div className="One_Card_Design">
       <p> {id} </p>
       <p> {name} </p>
       <p> {price} </p>
