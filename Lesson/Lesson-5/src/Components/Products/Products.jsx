aab0879a4f4103dd04defb8a285cd61e4c1c0651
ridwancodexr <ridwancodexr@gmail.com>
2026-02-15 09:12:38 +0600

    Products Components Create

---
+++ Lesson/Lesson-5/src/Components/Products/Products.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+
+const Products = () => {
+  const [singleProduct, setSingleProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("productsData.json")
+      .then((res) => res.json())
+      .then((data) => setSingleProduct(data));
+  }, []);
+
+  return (
+    <div>
+      {singleProduct.map((singleData) => (
+        <Product key={singleData.id} singleData={singleDataj}></Product>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
