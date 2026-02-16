b229e0c306f00cce35c4892e2474566a82770199
ridwancodexr <ridwancodexr@gmail.com>
2026-02-16 09:51:25 +0600

    react router setup: routing in react part-1

--- Lesson/Lesson-6/src/main.jsx
+++ Lesson/Lesson-6/src/main.jsx
@@ -10,6 +10,28 @@ const router = createBrowserRouter([
     path: "/",
     element: <div> Hello Ridwan </div>,
   },
+
+  {
+    path: "/about",
+    element: (
+      <div>
+        {" "}
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio
+          iste aut officia minima reprehenderit, enim commodi ducimus molestias
+          explicabo totam temporibus quas recusandae? Odit, praesentium autem ea
+          odio quaerat, dolor rerum amet reiciendis quibusdam illum
+          exercitationem molestias incidunt accusamus quasi quis nesciunt
+          tenetur laborum officia tempore debitis natus! Pariatur illum quaerat
+          dicta a voluptate, est reprehenderit neque voluptas aspernatur aliquam
+          iusto quidem explicabo consequatur mollitia vitae possimus sed
+          nesciunt omnis ipsam quos fugit nam quo. Laudantium veniam ea
+          sint!{" "}
+        </p>{" "}
+      </div>
+    ),
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
