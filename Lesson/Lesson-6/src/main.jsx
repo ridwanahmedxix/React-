ecba4660378348193717e1843cd5b370e59088fb
ridwancodexr <ridwancodexr@gmail.com>
2026-02-17 20:44:33 +0600

    some update

--- Lesson/Lesson-6/src/main.jsx
+++ Lesson/Lesson-6/src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div> Hello Ridwan </div>,
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 );
