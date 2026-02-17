e050c147200372dc1358dd72f71ab50ad0f9be6a
ridwancodexr <ridwancodexr@gmail.com>
2026-02-17 21:39:42 +0600

    Nav style add

--- Lesson/Lesson-6/src/Components/Navbar/Navbar.jsx
+++ Lesson/Lesson-6/src/Components/Navbar/Navbar.jsx
@@ -4,12 +4,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="bg-slate-600 p-[15px] ">
-      <Link to={"/home"}>Home</Link>
-      <Link to={"about"}>About</Link>
-      <Link to={"product"}>Product</Link>
-      <Link to={"/contact"}>Contact</Link>
-
-      {/* className="bg-purple-500 mx-3 p-[10px] rounded " */}
+      <Link className="bg-purple-500 mx-3 p-[10px] rounded " to={"/home"}>
+        Home
+      </Link>
+      <Link className="bg-purple-500 mx-3 p-[10px] rounded " to={"about"}>
+        About
+      </Link>
+      <Link className="bg-purple-500 mx-3 p-[10px] rounded " to={"product"}>
+        Product
+      </Link>
+      <Link className="bg-purple-500 mx-3 p-[10px] rounded " to={"/contact"}>
+        Contact
+      </Link>
     </div>
   );
 };
