c4376f15e15977eecb407cd140e5272c0017e850
ridwancodexr <ridwancodexr@gmail.com>
2026-02-17 21:01:13 +0600

    Home Component Create

---
+++ Lesson/Lesson-6/src/Components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="bg-pink-500 p-[10px] text-4xl ">
+        Welcome To The Home Page{" "}
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+        veritatis quidem earum, similique reiciendis quia atque nemo quas sed
+        delectus inventore voluptates sapiente et consequatur eaque, illo
+        labore, dolores modi id sint cumque. Aliquid dolores libero tempora iste
+        unde ducimus impedit, eius, molestias accusantium alias rem architecto
+        quidem commodi totam.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
--- Lesson/Lesson-6/src/Components/Navbar/Navbar.jsx
+++ Lesson/Lesson-6/src/Components/Navbar/Navbar.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="bg-slate-600 p-[15px] ">
-      <a className="bg-purple-500 mx-3 p-[10px] rounded " href="#">
+      <a className="bg-purple-500 mx-3 p-[10px] rounded " href="">
         Home
       </a>
-      <a className="bg-purple-500 mx-3 p-[10px] rounded " href="#">
+      <a className="bg-purple-500 mx-3 p-[10px] rounded " href="">
         About
       </a>
-      <a className="bg-purple-500 mx-3 p-[10px] rounded " href="#">
+      <a className="bg-purple-500 mx-3 p-[10px] rounded " href="">
         Products
       </a>
-      <a className="bg-purple-500 mx-3 p-[10px] rounded  " href="#">
+      <a className="bg-purple-500 mx-3 p-[10px] rounded  " href="">
         Contact
       </a>
     </div>
