2c69a27146dc4f61c184676991030a376d42cf62
ridwancodexr <ridwancodexr@gmail.com>
2026-02-17 08:20:35 +0600

    Tailwind css install

--- Lesson/Lesson-6/src/App.jsx
+++ Lesson/Lesson-6/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>Hello World </h1>
+      <h1 className="bg-red-500">Hello World </h1>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ Lesson/Lesson-6/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
