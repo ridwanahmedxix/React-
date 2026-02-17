59dae11e97a136248ab9638b59dd8492d9db740d
ridwancodexr <ridwancodexr@gmail.com>
2026-02-17 08:22:02 +0600

    some update

--- Lesson/Lesson-6/src/Components/Navbar/Navbar.jsx
+++ Lesson/Lesson-6/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <h1 className="text-red-500">Navbar</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
