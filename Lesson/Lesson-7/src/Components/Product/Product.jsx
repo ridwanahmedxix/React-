967dcb597588d2056a05688f65e8a5f8633ae183
ridwancodexr <ridwancodexr@gmail.com>
2026-02-18 21:55:52 +0600

    Card Design Done

--- Lesson/Lesson-7/src/Components/Product/Product.jsx
+++ Lesson/Lesson-7/src/Components/Product/Product.jsx
@@ -4,12 +4,13 @@ const Product = ({ singleData }) => {
   const { id, name, price, brand, color } = singleData;
 
   return (
-    <div>
+    <div className="bg-green-500 py-5 ">
       <p> Id - {id} </p>
       <p> Name - {name} </p>
       <p> Price - {price} </p>
       <p> Price - {brand} </p>
       <p> Price - {color} </p>
+      <button className="bg-purple-500 py-2 px-4 mt-4 "> Add To Card </button>
     </div>
   );
 };
--- Lesson/Lesson-7/src/Components/Products/Products.jsx
+++ Lesson/Lesson-7/src/Components/Products/Products.jsx
@@ -10,7 +10,7 @@ const Products = () => {
   }, []);
 
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-[30px]">
       {singleProduct.map((singleData) => (
         <Product key={singleData.id} singleData={singleData}></Product>
       ))}
