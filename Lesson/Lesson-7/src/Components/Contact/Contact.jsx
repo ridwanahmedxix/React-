05347b886638e9f956a6edc3e4464f7f42634695
ridwancodexr <ridwancodexr@gmail.com>
2026-02-18 22:08:05 +0600

    Component Create - Navbar , Contact , Home

---
+++ Lesson/Lesson-7/src/Components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Welcome to the Contact Page </h1>
+      <ul>
+        <li>Home - Dhaka , Bangladesh</li>
+        <li> Phone - 01900000000</li>
+        <li> It Codex R </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ Lesson/Lesson-7/src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is a Home Page </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, sit.
+        Laborum quisquam voluptates provident eveniet, totam excepturi aut ad
+        veritatis?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
