8e5fd037d517a6e718bea98baa614a2ce401d1bc
ridwancodexr <ridwancodexr@gmail.com>
2026-02-20 21:31:48 +0600

    Component Create - navbar , home , contact

---
+++ Projects/project-1/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Links } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      <Links> Home </Links>
+      <Links> Products </Links>
+      <Links> Contact </Links>
+    </div>
+  );
+};
+
+export default NavBar;
