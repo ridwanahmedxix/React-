0a3ffff973f6f8f1f15b80c7d95b33194381d8e9
ridwancodexr <ridwancodexr@gmail.com>
2026-02-20 10:58:59 +0600

    product card design

--- Projects/project-1/src/Components/Product/Product.jsx
+++ Projects/project-1/src/Components/Product/Product.jsx
@@ -4,12 +4,15 @@ const Product = ({ singleProduct }) => {
   const { id, name, price, brand, color } = singleProduct;
 
   return (
-    <div>
+    <div className="border-[2px] border-emerald-500 py-4 ">
       <p> Id - {id} </p>
       <p> Name - {name} </p>
       <p> Price - {price} </p>
       <p> Brand - {brand} </p>
       <p> Color - {color} </p>
+      <button className="bg-purple-500 py-2 px-3 mt-2 text-white ">
+        Add To Card
+      </button>
     </div>
   );
 };
