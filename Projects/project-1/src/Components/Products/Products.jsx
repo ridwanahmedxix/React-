756e212a6fb7790128e4ec6a9a6fa82d95411188
ridwancodexr <ridwancodexr@gmail.com>
2026-02-20 10:47:09 +0600

    Product component create

--- Projects/project-1/src/Components/Products/Products.jsx
+++ Projects/project-1/src/Components/Products/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 
 const Products = () => {
   const [productData, setProductData] = useState([]);
@@ -11,6 +12,9 @@ const Products = () => {
   return (
     <div>
       <h1>Products</h1>
+      {productData.map((singleProduct) => (
+        <Product key={singleProduct.id} singleProduct={singleProduct}></Product>
+      ))}
     </div>
   );
 };
