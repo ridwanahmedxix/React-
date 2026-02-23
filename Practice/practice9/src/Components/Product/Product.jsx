c4a75f7318d3d82b4fd60bee036e7773ca8dcb51
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-23 22:34:09 +0600

    products & product component code write done

--- Practice/practice9/src/Components/Product/Product.jsx
+++ Practice/practice9/src/Components/Product/Product.jsx
@@ -6,6 +6,10 @@ const Product = ({ singleProduct }) => {
   return (
     <div>
       <p> Id - {id} </p>
+      <p> Name - {name} </p>
+      <p> Price - {price} </p>
+      <p> Brand - {brand} </p>
+      <p> Color - {color} </p>
     </div>
   );
 };
