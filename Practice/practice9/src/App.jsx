30246660b4026023c46836acc6218fea7fa0ff09
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-24 21:20:51 +0600

    path link add

--- Practice/practice9/src/App.jsx
+++ Practice/practice9/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Appex from "./Components/Appex/Appex";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <Appex></Appex>
+      <Navbar></Navbar>
     </div>
   );
 }
--- Practice/practice9/src/main.jsx
+++ Practice/practice9/src/main.jsx
@@ -3,11 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home/Home.jsx";
+import Appex from "./Components/Appex/Appex.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    children: [
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/products",
+        element: <Appex></Appex>,
+      },
+    ],
   },
 ]);
 
