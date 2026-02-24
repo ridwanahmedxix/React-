e9ec7a936629e770ecb0bf5c584e663ce74c42f0
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-24 21:07:42 +0600

    Navbar code write

---
+++ Practice/practice9/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link> Home </Link>
+      <Link> Products </Link>
+      <Link> Contact </Link>
+    </div>
+  );
+};
+
+export default Navbar;
