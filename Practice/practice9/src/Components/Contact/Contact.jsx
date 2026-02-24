9598342fcd25152fc7d52a99c76fec6bb980c991
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-24 21:14:36 +0600

    contact section code write done

---
+++ Practice/practice9/src/Components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Section</h1>
+      <ul>
+        <li>Location - Gulshan 2 </li>
+        <li> Phone - 019xxxxxxxx </li>
+        <li> It Codex R </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
