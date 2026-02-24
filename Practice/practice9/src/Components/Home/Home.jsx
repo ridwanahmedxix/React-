ef1fdc5c6729b6c0130850c2d7a33eeb7be8a3b6
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-24 21:09:54 +0600

    home compo code write done

---
+++ Practice/practice9/src/Components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home page </h1>
+      <br />
+      <p>
+        {" "}
+        <b>
+          {" "}
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. At,
+          quo.{" "}
+        </b>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        accusantium aliquam laboriosam ipsam eveniet enim voluptatibus natus,
+        libero consequatur? Aperiam animi velit nesciunt, eius nihil ullam
+        aliquam dignissimos aut eveniet ipsum voluptates fuga mollitia,
+        accusamus doloribus accusantium odit voluptatibus dolore consequatur
+        maxime, debitis officia. Sunt consequuntur officiis maxime unde
+        ipsa.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Home;
