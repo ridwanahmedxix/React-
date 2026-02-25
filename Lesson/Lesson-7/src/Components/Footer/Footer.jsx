ce63161a72cae865141a05c8a05bb2f4950ac7ea
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-25 23:07:51 +0600

    footer component create

---
+++ Lesson/Lesson-7/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <ul className="flex ml-5">
+        <li>IT CODEX R</li>
+        <li>Welcome to footer section</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
