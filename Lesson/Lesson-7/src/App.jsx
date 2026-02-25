5d8f32dc38db55146045330791cc116d32dece98
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-25 23:09:48 +0600

    some update

--- Lesson/Lesson-7/src/App.jsx
+++ Lesson/Lesson-7/src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Products from "./Components/Products/Products";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 }
--- Lesson/Lesson-7/src/Components/Footer/Footer.jsx
+++ Lesson/Lesson-7/src/Components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <ul className="flex ml-5">
+    <div className="mt-8  ">
+      <ul className="flex ml-5 text-center ">
         <li>IT CODEX R</li>
         <li>Welcome to footer section</li>
       </ul>
