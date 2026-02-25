02f1c4c48b4e6f6b781938cd45f88b4b244df66d
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-25 22:47:52 +0600

    contact section design add

--- Practice/practice9/src/Components/Contact/Contact.jsx
+++ Practice/practice9/src/Components/Contact/Contact.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="border-[4px] border-red-500 py-8 ">
       <h1>Contact Section</h1>
       <ul>
-        <li>Location - Gulshan 2 </li>
-        <li> Phone - 019xxxxxxxx </li>
-        <li> It Codex R </li>
+        <li className=" border-[2px] border-green-800 py-3 px-4 m-3">
+          Location - Gulshan 2{" "}
+        </li>
+        <li className=" border-[2px] border-green-800 py-3 px-4 m-3">
+          {" "}
+          Phone - 019xxxxxxxx{" "}
+        </li>
+        <li className=" border-[2px] border-green-800 py-3 px-4">It Codex R</li>
       </ul>
     </div>
   );
