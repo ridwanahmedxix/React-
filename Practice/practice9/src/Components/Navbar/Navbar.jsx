355c3466288a322bfc94c985df0a8159e224cae7
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-25 22:42:58 +0600

    navbar design add

--- Practice/practice9/src/Components/Navbar/Navbar.jsx
+++ Practice/practice9/src/Components/Navbar/Navbar.jsx
@@ -3,10 +3,19 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="bg-purple-600 py-5 px-6 text-white">
-      <Link to={"home"}> Home </Link>
-      <Link to={"products"}> Products </Link>
-      <Link to={"contact"}> Contact </Link>
+    <div className="bg-purple-600 py-5 px-6 text-white mb-7">
+      <Link className="px-4 text-3xl" to={"home"}>
+        {" "}
+        Home{" "}
+      </Link>
+      <Link className="px-4 text-3xl" to={"products"}>
+        {" "}
+        Products{" "}
+      </Link>
+      <Link className="px-4 text-3xl" to={"contact"}>
+        {" "}
+        Contact{" "}
+      </Link>
     </div>
   );
 };
