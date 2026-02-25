9d60d8a990b9340249e0eea91bd6978abcdfa00b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-25 22:41:39 +0600

    first commit

--- Practice/practice9/src/Components/Navbar/Navbar.jsx
+++ Practice/practice9/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="bg-purple-600 py-5 px-6 text-white">
       <Link to={"home"}> Home </Link>
       <Link to={"products"}> Products </Link>
       <Link to={"contact"}> Contact </Link>
