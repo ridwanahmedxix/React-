3eb10ba7d745162f697445542d2c8ea540cf8640
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-26 19:58:48 +0600

    navbar component create

--- Lesson/Lesson-8/src/App.jsx
+++ Lesson/Lesson-8/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="bg-red-400">Hello Wolrd</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ Lesson/Lesson-8/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="bg-green-300 px-5 py-4 ">
+      <Link to={""} className="ml-3 text-3xl">
+        {" "}
+        Home{" "}
+      </Link>
+      <Link to={""} className="ml-3 text-3xl">
+        {" "}
+        About{" "}
+      </Link>
+      <Link to={""} className="ml-3 text-3xl">
+        {" "}
+        Products{" "}
+      </Link>
+      <Link to={""} className="ml-3 text-3xl">
+        {" "}
+        Contact{" "}
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
