a4fd106a90d2348e0a279cf9aed5840267c81d90
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-02-26 20:13:26 +0600

    Footer component create

--- Lesson/Lesson-8/src/App.jsx
+++ Lesson/Lesson-8/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ Lesson/Lesson-8/src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-3 bg-gray-600 text-white mt-12 ">
+      © 2026 Your Website. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
