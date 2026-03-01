4d5a1bb24b420215cf7358a464ca133a31f3d508
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-01 11:25:22 +0600

    some code update

--- Lesson/Lesson-8/src/main.jsx
+++ Lesson/Lesson-8/src/main.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <Products></Products>,
       },
       {
-        path: "/product/productName",
+        path: "/product/2",
         element: <MoreInfo></MoreInfo>,
       },
       {
