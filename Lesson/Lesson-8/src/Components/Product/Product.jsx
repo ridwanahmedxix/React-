43d29991bf0a122400b147ed2dc25ede6ded26eb
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-01 11:13:31 +0600

    First commit

--- Lesson/Lesson-8/src/Components/Product/Product.jsx
+++ Lesson/Lesson-8/src/Components/Product/Product.jsx
@@ -9,6 +9,7 @@ const Product = ({ product }) => {
       <p> Name - {name} </p>
       <p> Email - {email} </p>
       <p> Address - {address.city} </p>
+      <button className="bg-red-400 py-2 px-4 mt-3 rounded">More Info</button>
     </div>
   );
 };
