77a751aa0eb5611ccc096fec07ac3f98eba1f716
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-03 12:13:17 +0600

    products - useState & useEffect import

--- Projects/project-2/src/Components/Products/Products.jsx
+++ Projects/project-2/src/Components/Products/Products.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
+  const [singleProduct, setSingleProduct] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setSingleProduct(data));
+  }, []);
+
   return (
     <div>
       <h1>Products</h1>
