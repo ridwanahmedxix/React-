75d6ac45cef9cdf5f4d4b19b7f4c3747610543ac
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-03 12:08:59 +0600

    products data add json file

---
+++ Projects/project-2/src/Components/Appex/Appex.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Products from "../Products/Products";
+
+const Appex = () => {
+  return (
+    <div>
+      <h1>Hello Appex</h1>
+      <Products></Products>
+    </div>
+  );
+};
+
+export default Appex;
