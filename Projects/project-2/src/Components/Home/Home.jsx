e0e2e048eb58766b4ad904ba2d32957db12dca45
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-03 12:31:39 +0600

    Home component create

---
+++ Projects/project-2/src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Welcome to the Home page </h1> <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
+        earum architecto quidem aspernatur laboriosam. Tenetur nisi impedit
+        aperiam ipsum ad.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
--- Projects/project-2/src/Components/Navbar/Navbar.jsx
+++ Projects/project-2/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <Link> Home </Link>
+      <Link> Products </Link>
+      <Link> Contact </Link>
     </div>
   );
 };
