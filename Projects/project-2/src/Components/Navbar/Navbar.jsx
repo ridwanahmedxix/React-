179d0bfeb9aae4b046ee2bcc9a3feb2c64993d24
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-04 11:32:16 +0600

    Navbar design

--- Projects/project-2/src/Components/Navbar/Navbar.jsx
+++ Projects/project-2/src/Components/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link> Home </Link>
-      <Link> Products </Link>
-      <Link> Contact </Link>
+    <div className="bg-purple-600 py-2 px-4 rounded-[15px] ">
+      <Link className="text-white text-2xl ml-4"> Home </Link>
+      <Link className="text-white text-2xl ml-4"> Products </Link>
+      <Link className="text-white text-2xl ml-4"> Contact </Link>
     </div>
   );
 };
