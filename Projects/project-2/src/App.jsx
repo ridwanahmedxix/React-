51afdc6870c8aea462bb498fbc24e578f331f3ea
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-04 11:29:46 +0600

    first commit

--- Projects/project-2/src/App.jsx
+++ Projects/project-2/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Appex from "./Components/Appex/Appex";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="bg-red-500">Hello World</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
