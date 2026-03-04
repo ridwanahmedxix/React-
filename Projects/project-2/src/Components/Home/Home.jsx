8bd4bb348a9b16b3844bd940f153614471db7d03
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-04 11:43:10 +0600

    home route design

--- Projects/project-2/src/Components/Home/Home.jsx
+++ Projects/project-2/src/Components/Home/Home.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1> Welcome to the Home page </h1> <br />
-      <p>
+      <h1 className="text-5xl text-pink-500 my-4">
+        {" "}
+        Welcome to the Home page{" "}
+      </h1>{" "}
+      <br />
+      <p className="text-2xl text-blue-600">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
         earum architecto quidem aspernatur laboriosam. Tenetur nisi impedit
         aperiam ipsum ad.
