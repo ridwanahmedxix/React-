fe09371071c19f2816b5be514a2de986943365fd
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-05 22:48:43 +0600

    button some update

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 const ConditionalStyle = () => {
-  const [isSpecial, setIsSpecial] = useState([]);
+  const [isSpecial, setIsSpecial] = useState(false);
 
   const toggleButton = () => {
-    console.log("Clicked");
+    setIsSpecial(!isSpecial);
   };
 
   return (
@@ -13,10 +13,10 @@ const ConditionalStyle = () => {
         onClick={toggleButton}
         className="bg-purple-500 py-2 px-4 text-white text-2xl rounded-[10px] mb-4 "
       >
-        Click Me
+        MAKE IT {isSpecial ? " NORMAL " : " SPECIAL "}
       </button>
       <div className="border-[3px] border-gray-950 p-[15px] m-[10px] bg-stone-400 ">
-        This Is A Normal Style
+        This Is A {isSpecial ? "Normal" : "Special"} Style
       </div>
     </div>
   );
