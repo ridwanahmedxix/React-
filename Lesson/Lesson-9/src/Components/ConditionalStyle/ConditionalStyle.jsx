0bd9cf2ed3c58ef819feda1dd2e182afd6c79d12
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-05 22:34:06 +0600

    div create

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -6,7 +6,9 @@ const ConditionalStyle = () => {
       <button className="bg-purple-500 py-2 px-4 text-white text-2xl rounded-[10px] mb-4 ">
         Click Me
       </button>
-      <div>This Is A Normal Style</div>
+      <div className="border-[3px] border-gray-950 p-[15px] m-[10px] bg-stone-400 ">
+        This Is A Normal Style
+      </div>
     </div>
   );
 };
