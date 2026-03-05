c3cacebf045366f6bdfcdf26b004ef0384f29cf3
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-05 22:29:38 +0600

    button create

--- Lesson/Lesson-9/src/App.jsx
+++ Lesson/Lesson-9/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import ConditionalStyle from "./Components/ConditionalStyle/ConditionalStyle";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <ConditionalStyle></ConditionalStyle>
     </div>
   );
 }
--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const ConditionalStyle = () => {
   return (
     <div>
-      <h1>ConditionalStyle </h1>
+      <button className="bg-purple-500 py-2 px-4 text-white text-2xl rounded-[10px] mb-4 ">
+        Click Me
+      </button>
+      <div>This Is A Normal Style</div>
     </div>
   );
 };
