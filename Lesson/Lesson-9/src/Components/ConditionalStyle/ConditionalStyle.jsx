3063bac9755e457781179d0371d922db58fc0645
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-05 22:38:36 +0600

    useState import

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ConditionalStyle = () => {
+  const [isSpecial, setIsSpecial] = useState([]);
+
+  const toggleButton = () => {
+    console.log("Clicked");
+  };
+
   return (
     <div>
-      <button className="bg-purple-500 py-2 px-4 text-white text-2xl rounded-[10px] mb-4 ">
+      <button
+        onClick={toggleButton}
+        className="bg-purple-500 py-2 px-4 text-white text-2xl rounded-[10px] mb-4 "
+      >
         Click Me
       </button>
       <div className="border-[3px] border-gray-950 p-[15px] m-[10px] bg-stone-400 ">
