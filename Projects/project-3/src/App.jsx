5f24b316b96aaa3607a9aaf063b5a2a35f10e476
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-09 22:30:02 +0600

    some update

--- Projects/project-3/src/App.jsx
+++ Projects/project-3/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, aperiam.
+      </p>
     </div>
   );
 }
