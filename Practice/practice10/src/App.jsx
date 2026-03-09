ec4bbf0954aba52ba7b9a3d380342551b5ff6277
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-09 10:25:07 +0600

    useState import

--- Practice/practice10/src/App.jsx
+++ Practice/practice10/src/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Products from "./Components/Products/Products";
+import PlaceMent from "./Components/PlaceMent/PlaceMent";
 
 function App() {
+  const [card, setCard] = useState([]);
+
   return (
     <div>
       <Products></Products>
+      <PlaceMent></PlaceMent>
     </div>
   );
 }
