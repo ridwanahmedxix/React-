a31972a1e3f996b46d840165c99ea702fa4aa765
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-10 22:00:20 +0600

    tailwind css install

--- Projects/project-3/src/App.jsx
+++ Projects/project-3/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
