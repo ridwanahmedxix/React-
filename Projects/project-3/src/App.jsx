8748693be12666b5e74095461dfc3947071da556
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-10 21:57:32 +0600

    first commit

--- Projects/project-3/src/App.jsx
+++ Projects/project-3/src/App.jsx
@@ -2,19 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, aperiam.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-        voluptatibus? Voluptates, odio. Expedita, quisquam. Similique dolore
-        molestias esse sapiente aut.
-      </p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
