9aba563740a6c02733956ae8fdd67a9f0e59cde9
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-11 21:46:20 +0600

    Navbar create

--- Projects/project-3/src/App.jsx
+++ Projects/project-3/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Appex from "./Components/Appex/Appex";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <Appex></Appex>
+      {/* <Appex></Appex> */}
+      <Navbar></Navbar>
     </div>
   );
 }
--- Projects/project-3/src/Components/Navbar/Navbar.jsx
+++ Projects/project-3/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="bg-blue-500 py-5 px-7 text-2xl text-white rounded-[20px]">
+      <Link to={"home"} className="mx-5 ">
+        {" "}
+        Home{" "}
+      </Link>
+      <Link to={"products"} className="mx-5 ">
+        {" "}
+        Products{" "}
+      </Link>
+      <Link to={"contact"} className="mx-5 ">
+        {" "}
+        Contact{" "}
+      </Link>
     </div>
   );
 };
