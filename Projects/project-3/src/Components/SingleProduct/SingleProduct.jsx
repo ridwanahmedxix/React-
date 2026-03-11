e4933ae59fce730ca9a6586a68c5725658a831f4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-11 21:29:08 +0600

    git add .

--- Projects/project-3/src/Components/SingleProduct/SingleProduct.jsx
+++ Projects/project-3/src/Components/SingleProduct/SingleProduct.jsx
@@ -4,7 +4,7 @@ const SingleProduct = ({ singleProductData }) => {
   const { id, name, price, brand, color } = singleProductData;
 
   return (
-    <div>
+    <div className="border-[3px] border-purple-400 my-4 p-6  rounded-[15px] ">
       <p> Id - {id} </p>
       <p> Name - {name} </p>
       <p> Price - {price} </p>
