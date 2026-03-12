505d372e9d6d030f7620806430da66747c59bf38
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-12 18:54:35 +0600

    Home component create

---
+++ Projects/project-3/src/Components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome To The Home Page</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
+        repellendus eum expedita! Fugiat, reprehenderit maiores perferendis rem
+        illo similique voluptatum! Aliquam reprehenderit repellat in porro
+        cumque voluptate rem soluta et dolorem, quisquam vero rerum saepe fugit,
+        blanditiis incidunt nostrum repudiandae veritatis consequuntur. Deleniti
+        ratione voluptatem optio perspiciatis fugiat sed voluptates!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
