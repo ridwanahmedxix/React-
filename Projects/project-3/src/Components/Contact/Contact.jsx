bf14a4bea6ac0b664409165ccdac139edf776a7c
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-12 18:59:54 +0600

    Contact component create

---
+++ Projects/project-3/src/Components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Section</h1>
+      <ul>
+        <li> Location - Gulshan , Dhaka </li>
+        <li> Phone - 6435342624 </li>
+        <li> HP ELITEBOOK BD </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
