dbb43cff5c0915af9501223ffc1bf14c2f5f5379
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-12 21:34:39 +0600

    new file setup

--- Lesson/Lesson-9/src/App.jsx
+++ Lesson/Lesson-9/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import ConditionalStyle from "./Components/ConditionalStyle/ConditionalStyle";
 
 function App() {
   return (
     <div>
-      <ConditionalStyle></ConditionalStyle>
+      <h1>Hello World</h1>
     </div>
   );
 }
