1d828aa369f3e63ec8b767dcc2ebd1123e06bfe7
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-12 21:46:02 +0600

    useState import

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ConditionalStyle = () => {
+  const [isSpecial, setIsSpecial] = useState();
+
+  const toggleButton = () => {
+    console.log("Clicked");
+  };
+
   return (
     <div>
-      <button className="bg-purple-600 text-white py-2 px-4 text-2xl rounded hover:bg-purple-700">
+      <button
+        onClick={toggleButton}
+        className="bg-purple-600 text-white py-2 px-4 text-2xl rounded hover:bg-purple-700"
+      >
         Make It Special
       </button>
       <div>This is a Normal Section</div>
