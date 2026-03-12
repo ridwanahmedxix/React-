2c1269da80e484896ef2353453df969b1a48d969
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-12 21:37:12 +0600

    ConditionalStyle component create

---
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ConditionalStyle = () => {
+  return (
+    <div>
+      <button>Make It Special</button>
+    </div>
+  );
+};
+
+export default ConditionalStyle;
