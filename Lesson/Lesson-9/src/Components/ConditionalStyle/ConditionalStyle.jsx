1dfd5627031d0aff05d5ef61a34d3910005dec42
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-12 21:40:31 +0600

    button and section create

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const ConditionalStyle = () => {
   return (
     <div>
-      <button>Make It Special</button>
+      <button className="bg-purple-600 text-white py-2 px-4 text-2xl rounded hover:bg-purple-700">
+        Make It Special
+      </button>
+      <div>This is a Normal Section</div>
     </div>
   );
 };
