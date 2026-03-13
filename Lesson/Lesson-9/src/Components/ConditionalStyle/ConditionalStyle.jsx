9631ebd8d63721f3b727eb8e90232e4a5bddc1c3
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 10:22:42 +0600

    useState import

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ConditionalStyle = () => {
+  const [isSpecial, setIsSpecial] = useState(false);
+
   return (
     <div>
       <h1>Hello World</h1>
