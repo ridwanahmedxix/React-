9314eaa87de5d9d368c3e1d0745504fbe785f965
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 10:21:43 +0600

    ConditionalStyle basic setup

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -1,31 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 const ConditionalStyle = () => {
-  const [isSpecial, setIsSpecial] = useState(false);
-
-  const toggleButton = () => {
-    setIsSpecial(!isSpecial);
-  };
-
   return (
     <div>
-      <button
-        onClick={toggleButton}
-        className="bg-purple-600 text-white py-2 px-4 text-2xl my-5 rounded hover:bg-purple-700"
-      >
-        Make It {isSpecial ? " Normal " : " Special "}
-      </button>
-      <div
-        className={
-          isSpecial
-            ? "bg-green-500 rounded-[10PX] text-2xl text-white p-4"
-            : "bg-red-500"
-        }
-      >
-        This is a {isSpecial ? "Special" : "Normal"} Section
-      </div>
+      <h1>Hello World</h1>
     </div>
   );
 };
-
 export default ConditionalStyle;
