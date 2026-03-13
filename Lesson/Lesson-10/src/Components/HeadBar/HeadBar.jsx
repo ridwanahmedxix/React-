a16ad6b803cc3924e2baf6e8c54d73fc0e1147f4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 19:31:23 +0600

    HeadBar component create

---
+++ Lesson/Lesson-10/src/Components/HeadBar/HeadBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeadBar = () => {
+  return (
+    <div>
+      <h1>HeadBar</h1>
+      <button> Click Me - HeadBar </button>
+    </div>
+  );
+};
+
+export default HeadBar;
