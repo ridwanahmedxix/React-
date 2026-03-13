cfd69d056a9955e80b4aef41d1cdc897862e823e
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 10:38:35 +0600

    clicked to normal and special button , div create

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -12,12 +12,20 @@ const ConditionalStyle = () => {
     <div>
       <button
         onClick={toggleButton}
-        className="bg-purple-500 text-white text-2xl py-3 px-5 rounded-[10px]  hover:bg-purple-600 "
+        className="bg-purple-500 text-white text-2xl py-3 px-5 rounded-[10px] mb-10  hover:bg-purple-600 "
       >
         Make It {isSpecial ? "Normal" : "Special"}
       </button>
 
-      <div>This Is A {isSpecial ? "Special" : "Normal"} Section </div>
+      <div
+        className={
+          isSpecial
+            ? "bg-green-300 py-10 px-7 text-2xl rounded-[15px] font-mono  text-white"
+            : "text-black text-2xl py-10 px-7  "
+        }
+      >
+        This Is A {isSpecial ? "Special" : "Normal"} Section{" "}
+      </div>
     </div>
   );
 };
