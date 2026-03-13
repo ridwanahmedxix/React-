e94c8e59269d8263d760a13f9c13c81d58e3358a
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 10:24:57 +0600

    button create

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -3,9 +3,14 @@ import React, { useState } from "react";
 const ConditionalStyle = () => {
   const [isSpecial, setIsSpecial] = useState(false);
 
+  const toggleButton = () => {
+    setIsSpecial(!isSpecial);
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
+      <button> Make it special </button>
     </div>
   );
 };
