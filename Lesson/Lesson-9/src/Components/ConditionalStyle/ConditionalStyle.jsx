c57edb1154a16cb2b7ce9ebd8b08026ee9b441fd
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 10:31:30 +0600

    Button design

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -10,8 +10,10 @@ const ConditionalStyle = () => {
 
   return (
     <div>
-      <h1>Hello World</h1>
-      <button> Make it special </button>
+      <button className="bg-purple-500 text-white text-2xl py-3 px-5 rounded-[10px]  hover:bg-purple-600 ">
+        {" "}
+        Make it special{" "}
+      </button>
     </div>
   );
 };
