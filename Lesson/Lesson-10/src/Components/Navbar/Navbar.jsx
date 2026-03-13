e0b15d21cf35da7809c195f793948a07dd126015
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 19:30:35 +0600

    Navbar component create

---
+++ Lesson/Lesson-10/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <button> Click Me - Navbar </button>
+    </div>
+  );
+};
+
+export default Navbar;
