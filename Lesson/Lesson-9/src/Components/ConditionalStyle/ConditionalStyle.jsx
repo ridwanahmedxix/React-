04b1460aaba907591a2833cf25ce48cd2866b96b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-13 10:33:19 +0600

    button some update

--- Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
+++ Lesson/Lesson-9/src/Components/ConditionalStyle/ConditionalStyle.jsx
@@ -10,9 +10,11 @@ const ConditionalStyle = () => {
 
   return (
     <div>
-      <button className="bg-purple-500 text-white text-2xl py-3 px-5 rounded-[10px]  hover:bg-purple-600 ">
-        {" "}
-        Make it special{" "}
+      <button
+        onClick={toggleButton}
+        className="bg-purple-500 text-white text-2xl py-3 px-5 rounded-[10px]  hover:bg-purple-600 "
+      >
+        Make It {isSpecial ? "Normal" : "Special"}
       </button>
     </div>
   );
