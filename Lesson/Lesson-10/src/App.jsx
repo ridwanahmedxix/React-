e7eb101acd8e6d4fe0e9d4f2b3565d1975a64d31
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-14 19:11:53 +0600

    some code update

--- Lesson/Lesson-10/src/App.jsx
+++ Lesson/Lesson-10/src/App.jsx
@@ -2,17 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>Hello World </h1>
-
-      {/* <button onClick={() => setCount(count + 1)}>Count is {count}</button> */}
-
-      <button> Count Number </button>
+      <button onClick={() => setCount(count + 1)}>Count Number {count} </button>
     </div>
   );
 }
