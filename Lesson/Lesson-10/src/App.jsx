0f7074d0c55411d31ce1600826af36db193f306b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-14 19:07:49 +0600

    count button create

--- Lesson/Lesson-10/src/App.jsx
+++ Lesson/Lesson-10/src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Hello World </h1>
+
+      <button onClick={() => setCount((count) => count + 1)}>
+        Count is {count}
+      </button>
     </div>
   );
 }
