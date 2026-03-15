65390fd21ce5d6e9b629ebc6996ca256d1ef0338
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-15 08:48:16 +0600

    button some update

--- Lesson/Lesson-10/src/Components/HeadBar/HeadBar.jsx
+++ Lesson/Lesson-10/src/Components/HeadBar/HeadBar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import LastNav from "../LastNav/LastNav";
 
 const HeadBar = ({ count }) => {
   return (
     <div>
+      <LastNav></LastNav>
       <button className="bg-yellow-400 text-white my-5 py-3 px-5 rounded-[15px]  ">
         Headbar - {count}
       </button>
--- Lesson/Lesson-10/src/Components/LastNav/LastNav.jsx
+++ Lesson/Lesson-10/src/Components/LastNav/LastNav.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const LastNav = () => {
   return (
     <div>
-      <button>LastNav - </button>
+      <button className="bg-purple-400 hover:bg-gray-500 transition text-white my-5 py-3 px-5 rounded-[15px]  ">
+        LastNav -{" "}
+      </button>
     </div>
   );
 };
