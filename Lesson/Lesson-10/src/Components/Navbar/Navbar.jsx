fa95ab6072da7ab615edef287012db1575fe36a2
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-15 08:53:11 +0600

    some update

--- Lesson/Lesson-10/src/Components/Navbar/Navbar.jsx
+++ Lesson/Lesson-10/src/Components/Navbar/Navbar.jsx
@@ -8,6 +8,10 @@ const Navbar = ({ count }) => {
       <button className="bg-red-400  hover:bg-gray-500 transition text-white my-5 py-3 px-5 rounded-[15px]  ">
         Navbar - {count}
       </button>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum,
+        consequatur.
+      </p>
     </div>
   );
 };
