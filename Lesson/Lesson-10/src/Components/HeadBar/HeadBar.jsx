7d2a536b7176f794abd885b0204c2166dcc8c677
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-15 09:32:40 +0600

    React Context API useContext hook

--- Lesson/Lesson-10/src/Components/HeadBar/HeadBar.jsx
+++ Lesson/Lesson-10/src/Components/HeadBar/HeadBar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import LastNav from "../LastNav/LastNav";
+import { CountContext } from "../../App";
 
 const HeadBar = ({ count }) => {
+  const countState = useContext(CountContext);
+
   return (
     <div>
       <LastNav></LastNav>
       <button className="bg-yellow-400  hover:bg-gray-500 transition text-white my-5 py-3 px-5 rounded-[15px]  ">
-        Headbar - {count}
+        Headbar - {countState}
       </button>
     </div>
   );
