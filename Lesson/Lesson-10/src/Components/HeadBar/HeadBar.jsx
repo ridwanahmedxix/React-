bcec793eb02626745699b4b5f487adafa78c40d5
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-15 08:43:33 +0600

    Headbar button design add

--- Lesson/Lesson-10/src/Components/HeadBar/HeadBar.jsx
+++ Lesson/Lesson-10/src/Components/HeadBar/HeadBar.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const HeadBar = ({ count }) => {
   return (
     <div>
-      <button> Click Me - {count} </button>
+      <button className="bg-yellow-400 text-white my-5 py-3 px-5 rounded-[15px]  ">
+        Click Me - {count}
+      </button>
     </div>
   );
 };
