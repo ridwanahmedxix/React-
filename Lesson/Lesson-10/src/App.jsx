ef1c37ab2fa8a751a6691410135d30044b999e1f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-15 09:21:40 +0600

    useContext

--- Lesson/Lesson-10/src/App.jsx
+++ Lesson/Lesson-10/src/App.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+
+export const CountContext = createContext();
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Navbar></Navbar>
-      <button
-        className="bg-green-400  hover:bg-gray-500 transition text-white my-5 py-3 px-5 rounded-[15px]  "
-        onClick={() => setCount(count + 1)}
-      >
-        Count Number {count}{" "}
-      </button>
+      <CountContext.Provider value={count}>
+        <Navbar></Navbar>
+        <button
+          className="bg-green-400  hover:bg-gray-500 transition text-white my-5 py-3 px-5 rounded-[15px]  "
+          onClick={() => setCount(count + 1)}
+        >
+          Count Number {count}{" "}
+        </button>
+      </CountContext.Provider>
     </div>
   );
 }
--- Lesson/Lesson-10/src/Components/LastNav/LastNav.jsx
+++ Lesson/Lesson-10/src/Components/LastNav/LastNav.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CountContext } from "../../App";
 
 const LastNav = () => {
+  const countState = useContext(CountContext);
+
   return (
     <div>
       <button className="bg-purple-400 hover:bg-gray-500 transition text-white my-5 py-3 px-5 rounded-[15px]  ">
-        LastNav -{" "}
+        LastNav - {countState}
       </button>
     </div>
   );
