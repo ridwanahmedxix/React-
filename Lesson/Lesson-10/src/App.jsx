111a73a5315e3a85066edcb18e19f1834b3a8598
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-15 08:40:36 +0600

    Navbar button show

--- Lesson/Lesson-10/src/App.jsx
+++ Lesson/Lesson-10/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Navbar></Navbar>
       <button
         className="bg-green-400 text-white py-3 px-5 rounded-[15px]  "
         onClick={() => setCount(count + 1)}
