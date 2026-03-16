e2f3cdf8e78f93ccc350d05dbb1005040a30e7bc
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-16 11:49:54 +0600

    last class code revision

--- Practice/practice8/src/Components/Navbar/Navbar.jsx
+++ Practice/practice8/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import HeadBar from "../HeadBar/HeadBar";
+import { CountContext } from "../../App";
 
 const Navbar = () => {
+  const CountState = useContext(CountContext);
+
   return (
     <div>
       <HeadBar></HeadBar>
       <button className="bg-yellow-400 hover:bg-slate-600 transition my-5 text-white py-3 px-5 text-2xl rounded-[15px]">
-        Navbar Button{" "}
+        Navbar Button - {CountState}
       </button>
     </div>
   );
