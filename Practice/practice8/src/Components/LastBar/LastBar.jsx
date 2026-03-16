81e84eaec45e3597614837b1f1b95a4e48393853
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-16 11:46:29 +0600

    one button click to two button count

--- Practice/practice8/src/Components/LastBar/LastBar.jsx
+++ Practice/practice8/src/Components/LastBar/LastBar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CountContext } from "../../App";
 
 const LastBar = () => {
+  const CountState = useContext(CountContext);
+
   return (
     <div>
       <button className="bg-blue-500 hover:bg-slate-600 transition my-5 text-white py-3 px-5 text-2xl rounded-[15px]">
-        LastBar Button -{" "}
+        LastBar Button - {CountState}
       </button>
     </div>
   );
