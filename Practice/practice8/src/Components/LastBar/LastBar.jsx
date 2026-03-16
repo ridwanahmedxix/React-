e7256eb58283ce90cdae12eed4664994ea40be81
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-16 12:16:19 +0600

    last class code revision

--- Practice/practice8/src/Components/LastBar/LastBar.jsx
+++ Practice/practice8/src/Components/LastBar/LastBar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CountContext } from "../../App";
 
 const LastBar = () => {
+  const countState = useContext(CountContext);
+
   return (
     <div>
       <button className="bg-green-400 hover:bg-slate-600 transition my-5 text-white py-3 px-5 text-2xl rounded-[15px]">
-        Lastbar
+        Lastbar - {countState}
       </button>
     </div>
   );
