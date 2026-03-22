4f7c90ec584c5f9ed19c16bd4a41cdf0b9fc67ba
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-22 19:52:06 +0600

    placement component create

--- Practice/practice11/src/App.jsx
+++ Practice/practice11/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Products from "./Components/Products/Products";
+import PlaceMent from "./Components/PlaceMent/PlaceMent";
 
 function App() {
   const [singleData, setSingleData] = useState([]);
@@ -10,8 +11,9 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="flex gap-5">
       <Products AddToCard={AddToCard}></Products>
+      <PlaceMent></PlaceMent>
     </div>
   );
 }
---
+++ Practice/practice11/src/Components/PlaceMent/PlaceMent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlaceMent = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-blue-500 m-4 ">Add To Card </h1>
+    </div>
+  );
+};
+
+export default PlaceMent;
