3517a86cb59ceb81a310cff933ce868aae08e01f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-22 19:01:09 +0600

    Product component create

--- Practice/practice11/src/Components/Products/Products.jsx
+++ Practice/practice11/src/Components/Products/Products.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+
   return (
     <div>
       <h1>Products</h1>
