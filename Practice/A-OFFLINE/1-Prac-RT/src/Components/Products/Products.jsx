f404814b5b7dc6b373facb87da679c5054476c06
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-26 19:25:07 +0600

    useState import

---
+++ Practice/A-OFFLINE/1-Prac-RT/src/Components/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Products = () => {
+  const [singleData, setSingleData] = useState([]);
+
+  return (
+    <div>
+      <h1>
+        https://keep.google.com/u/0/#NOTE/1ohornAc0esKLdJejIetyzNkIDJ9dYS8VG-SVouanL5TkcH--6tvXTQZKkssG1Q
+      </h1>
+    </div>
+  );
+};
+
+export default Products;
