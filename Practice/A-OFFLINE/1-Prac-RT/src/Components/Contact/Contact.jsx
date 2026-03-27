5d8588ea71ba90d854f7c8ba6398682eb0beb6b4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-27 18:58:20 +0600

    home and contact component create

---
+++ Practice/A-OFFLINE/1-Prac-RT/src/Components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <ul>
+        <li> Location - Dhaka Ghulshan </li>
+        <li> Phone - 6357253783542 </li>
+        <li> Email- example@gmail.com </li>
+        <li> Facebook - Test Facebook </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ Practice/A-OFFLINE/1-Prac-RT/src/Components/Home/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome To The Home Page </h1>
+      <p>
+        <b>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, aut?
+        </b>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quas porro
+        maiores sint quidem numquam ipsa asperiores, excepturi cum possimus!
+        <i>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit,
+          error?
+        </i>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, ex.
+      </p>
+      <br />
+      <br />
+      <p>
+        {" "}
+        <b>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+          aut?{" "}
+        </b>{" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quas porro
+        maiores sint quidem numquam ipsa asperiores, excepturi cum possimus!{" "}
+        <i>
+          {" "}
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit,
+          error?{" "}
+        </i>{" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, ex.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Home;
