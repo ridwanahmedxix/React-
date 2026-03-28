985245f79c78e61dde11f85837b33d2c09bc6897
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-28 10:08:04 +0600

    ThirdNav  component create

---
+++ Practice/practice12/src/Components/ThirdNav/ThirdNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ThirdNav = () => {
+  return (
+    <div>
+      <button className="bg-yellow-400 py-3 px-4 text-3xl rounded ">
+        ThirdNav
+      </button>
+    </div>
+  );
+};
+
+export default ThirdNav;
