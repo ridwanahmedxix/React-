ba1d4896551442e52d1b93d8111c58951ecf5047
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-28 09:53:57 +0600

    counetr app create

--- Practice/practice12/src/App.jsx
+++ Practice/practice12/src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1 className="bg-red-500">Hello World</h1>
+
+      <button onClick={() => setCount(count + 1)}> Counter - {count} </button>
     </div>
   );
 }
