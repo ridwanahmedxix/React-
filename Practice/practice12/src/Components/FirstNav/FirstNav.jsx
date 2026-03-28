0c37753b04f454c51032dd1a815b03b427dcd383
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-28 10:04:10 +0600

    Firstnav component create

---
+++ Practice/practice12/src/Components/FirstNav/FirstNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FirstNav = () => {
+  return (
+    <div>
+      <button className="bg-green-400 py-3 px-4 text-3xl rounded ">
+        FirstNav
+      </button>
+    </div>
+  );
+};
+
+export default FirstNav;
