2f7462e158dfa4903efdcae7a9b4cdb01e20a7e4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-28 10:06:27 +0600

    Secondnav component create

--- Practice/practice12/src/Components/FirstNav/FirstNav.jsx
+++ Practice/practice12/src/Components/FirstNav/FirstNav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FirstNav = () => {
   return (
     <div>
-      <button className="bg-green-400 py-3 px-4 text-3xl rounded ">
+      <button className="bg-blue-400 py-3 px-4 text-3xl rounded ">
         FirstNav
       </button>
     </div>
---
+++ Practice/practice12/src/Components/SecondNav/SecondNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SecondNav = () => {
+  return (
+    <div>
+      <button className="bg-red-400 py-3 px-4 text-3xl rounded ">
+        SecondNav
+      </button>
+    </div>
+  );
+};
+
+export default SecondNav;
