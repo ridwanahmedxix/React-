88ac5489a04fa9e1c1c413667dd165a6e46616f2
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-29 20:05:03 +0600

    some code clear

--- Practice/practice12/src/App.jsx
+++ Practice/practice12/src/App.jsx
@@ -1,19 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import FirstNav from "./Components/FirstNav/FirstNav";
-import { ContextOne } from "./Contexts/Context1";
 
 function App() {
-  const [datA, setDatA] = useState("Hello World");
-
   return (
     <div>
-      <ContextOne.Provider value={[datA, setDatA]}>
-        <FirstNav></FirstNav>
-        <button className="bg-teal-400 py-3 px-4  my-4 text-3xl rounded ">
-          Main
-        </button>
-      </ContextOne.Provider>
+      <FirstNav></FirstNav>
+      <button className="bg-teal-400 py-3 px-4  my-4 text-3xl rounded ">
+        Main
+      </button>
     </div>
   );
 }
--- Practice/practice12/src/Contexts/Context1.jsx
+++ Practice/practice12/src/Contexts/Context1.jsx
@@ -1,3 +1,5 @@
 import { createContext } from "react";
 
 export const ContextOne = createContext();
+
+const [datA, setDatA] = useState("Hello World");
