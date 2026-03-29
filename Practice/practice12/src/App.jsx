6841278f1cdfa0253138180632b184bdffee78cf
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-29 09:09:05 +0600

    useState import

--- Practice/practice12/src/App.jsx
+++ Practice/practice12/src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import FirstNav from "./Components/FirstNav/FirstNav";
+import { ContextOne } from "./Contexts/Context1";
 
 function App() {
+  const [dataa, setDataa] = useState("Hello World");
+
   return (
     <div>
-      <FirstNav></FirstNav>
-      <button className="bg-blue-400 py-3 px-4 text-3xl  my-4 rounded ">
-        Main
-      </button>
+      <ContextOne.Provider value={dd}>
+        <FirstNav></FirstNav>
+        <button className="bg-blue-400 py-3 px-4 text-3xl  my-4 rounded ">
+          Main
+        </button>
+      </ContextOne.Provider>
     </div>
   );
 }
