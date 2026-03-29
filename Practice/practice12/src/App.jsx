b706327f3671206819df0935634643d182b6e25a
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-29 20:01:11 +0600

    data send

--- Practice/practice12/src/App.jsx
+++ Practice/practice12/src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import FirstNav from "./Components/FirstNav/FirstNav";
+import { ContextOne } from "./Contexts/Context1";
 
 function App() {
+  const [datA, setDatA] = useState("Hello World");
+
   return (
     <div>
-      <FirstNav></FirstNav>
-      <button className="bg-teal-400 py-3 px-4  my-4 text-3xl rounded ">
-        Main
-      </button>
+      <ContextOne.Provider value={[datA, setDatA]}>
+        <FirstNav></FirstNav>
+        <button className="bg-teal-400 py-3 px-4  my-4 text-3xl rounded ">
+          Main
+        </button>
+      </ContextOne.Provider>
     </div>
   );
 }
--- Practice/practice12/src/Components/ThirdNav/ThirdNav.jsx
+++ Practice/practice12/src/Components/ThirdNav/ThirdNav.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContextOne } from "../../Contexts/Context1";
 
 const ThirdNav = () => {
+  const [datA, setDatA] = useContext(ContextOne);
+
   return (
     <div>
       <button className="bg-red-400 py-3 px-4  my-4 text-3xl rounded ">
-        ThirdNav
+        ThirdNav - {datA}
       </button>
     </div>
   );
