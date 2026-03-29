d6b8adcc4e3e01e9720ff42fe288b9eb5633d6b4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-29 08:20:15 +0600

    context provide

--- Practice/practice12/src/App.jsx
+++ Practice/practice12/src/App.jsx
@@ -11,15 +11,17 @@ function App() {
 
   return (
     <div>
-      <CountContext.Provider value={[count, setCount]}>
-        <FirstNav></FirstNav>
-        <button
-          className="bg-blue-400 py-3 px-4 text-3xl my-3 rounded "
-          onClick={() => setCount(count + 1)}
-        >
-          Count - {count}
-        </button>
-      </CountContext.Provider>
+      <AnotherContext.Provider value={[another, setAnother]}>
+        <CountContext.Provider value={[count, setCount]}>
+          <FirstNav></FirstNav>
+          <button
+            className="bg-blue-400 py-3 px-4 text-3xl my-3 rounded "
+            onClick={() => setCount(count + 1)}
+          >
+            Count - {count}
+          </button>
+        </CountContext.Provider>
+      </AnotherContext.Provider>
     </div>
   );
 }
