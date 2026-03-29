4d8b0d5c113a4297b24c88e0483001b8c291cd59
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-29 19:50:41 +0600

    revision code file basic setup

---
+++ Practice/practice12/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button className="bg-teal-400 py-3 px-4  my-4 text-3xl rounded ">
+        Main
+      </button>
+    </div>
+  );
+}
+export default App;
---
+++ Practice/practice12/src/Components/ThirdNav/ThirdNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ThirdNav = () => {
+  return (
+    <div>
+      <button className="bg-red-400 py-3 px-4  my-4 text-3xl rounded ">
+        ThirdNav
+      </button>
+    </div>
+  );
+};
+
+export default ThirdNav;
