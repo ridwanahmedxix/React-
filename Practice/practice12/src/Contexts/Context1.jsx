102634366c5a6e84db6eed6ff033ab97e8287b1a
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-30 07:06:42 +0600

    useState import

--- Practice/practice12/src/Contexts/Context1.jsx
+++ Practice/practice12/src/Contexts/Context1.jsx
@@ -1,3 +1,11 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const ContextOne = createContext();
+
+export const Context1Provider = () => {
+  const [datA, setDatA] = useState("Ridwan Ahmed");
+
+  return;
+
+  <h1>Hello World</h1>;
+};
