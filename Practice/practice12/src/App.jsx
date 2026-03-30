3237a56b321617845b5d5c60d6d9a371952f48e9
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-30 06:59:09 +0600

    all code clear

--- Practice/practice12/src/App.jsx
+++ Practice/practice12/src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import "./App.css";
 import FirstNav from "./Components/FirstNav/FirstNav";
-import { ContextProvider } from "./Contexts/Context1";
 
 function App() {
   return (
     <div>
-      <ContextProvider>
-        <FirstNav></FirstNav>
-        <button className="bg-teal-400 py-3 px-4  my-4 text-3xl rounded ">
-          Main
-        </button>
-      </ContextProvider>
+      <FirstNav></FirstNav>
+      <button className="bg-teal-400 py-3 px-4  my-4 text-3xl rounded ">
+        Main
+      </button>
     </div>
   );
 }
--- Practice/practice12/src/Components/ThirdNav/ThirdNav.jsx
+++ Practice/practice12/src/Components/ThirdNav/ThirdNav.jsx
@@ -1,13 +1,10 @@
-import React, { useContext } from "react";
-import { ContextOne } from "../../Contexts/Context1";
+import React from "react";
 
 const ThirdNav = () => {
-  const [datA, setDatA] = useContext(ContextOne);
-
   return (
     <div>
       <button className="bg-red-400 py-3 px-4  my-4 text-3xl rounded ">
-        ThirdNav - {}
+        ThirdNav -
       </button>
     </div>
   );
