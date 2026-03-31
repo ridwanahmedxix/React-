8ecae6f06c243930261bcf04e5045499fa749f95
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-31 19:20:42 +0600

    function new update

--- Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
+++ Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
@@ -2,22 +2,44 @@ import React, { useState } from "react";
 
 const SubmitForm = () => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const HandleSubmitForm = (e) => {
     e.preventDefault();
     console.log(name);
+    console.log(email);
+    console.log(password);
   };
 
   return (
     <div>
       <form onSubmit={HandleSubmitForm}>
-        <input type="text" name="text" />
+        <input
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          type="text"
+          name="text"
+        />
         <br />
         <br />
-        <input type="email" name="email" />
+        <input
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="email"
+          name="email"
+        />
         <br />
         <br />
-        <input type="password" name="password" />
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+          name="password"
+        />
         <br />
         <br />
         <input type="submit" value="SUBMIT" />
