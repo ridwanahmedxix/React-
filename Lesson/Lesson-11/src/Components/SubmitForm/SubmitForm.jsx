9c2d926a7220fd838a11dfb2d20e684c89a4e656
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-03-31 07:32:49 +0600

    App.jsx basic setup

---
+++ Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SubmitForm = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="" />
+        <br />
+        <br />
+        <input type="email" name="" />
+        <br />
+        <br />
+        <input type="password" />
+        <br />
+        <br />
+      </form>
+    </div>
+  );
+};
+
+export default SubmitForm;
