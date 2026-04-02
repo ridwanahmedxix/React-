01250521f0db6d41ef2a3642c63e4ef660438aa8
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-02 16:08:45 +0600

    all update

--- Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
+++ Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
@@ -1,18 +1,45 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const SubmitForm = () => {
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passRef = useRef("");
+  const [error, setError] = useState("");
+
+  const HandleSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (passRef.current.value.lenght < 8) {
+      setError("Password must be 8 characters ");
+    } else {
+      setError("");
+      console.log(nameRef.current.value);
+      console.log(emailRef.current.value);
+      console.log(passRef.current.value);
+    }
+  };
+
   return (
     <div>
       <form onSubmit={HandleSubmitForm}>
-        <input type="text" name="text" />
+        <input ref={nameRef} type="text" name="text" />
         <br />
         <br />
-        <input type="email" name="email" />
+        <input
+          ref={emailRef}
+          defaultValue={"example@gmail.com"}
+          type="email"
+          name="email"
+        />
         <br />
         <br />
-        <input type="password" name="password" />
+        <input ref={passRef} type="password" name="password" />
         <br />
         <br />
+
+        <p> {error} </p>
+        <br />
+
         <input type="submit" value="SUBMIT" />
       </form>
     </div>
