6545307697ee6de6dc2bcb9f7030f51298c97c1c
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-02 16:26:07 +0600

    function delete

--- Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
+++ Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
@@ -1,24 +1,6 @@
 import React, { useRef, useState } from "react";
 
 const SubmitForm = () => {
-  const nameRef = useRef("");
-  const emailRef = useRef("");
-  const passRef = useRef("");
-  const [error, setError] = useState("");
-
-  const HandleSubmitForm = (e) => {
-    e.preventDefault();
-
-    if (passRef.current.value.lenght < 8) {
-      setError("Password must be 8 characters ");
-    } else {
-      setError("");
-      console.log(nameRef.current.value);
-      console.log(emailRef.current.value);
-      console.log(passRef.current.value);
-    }
-  };
-
   return (
     <div>
       <form onSubmit={HandleSubmitForm}>
