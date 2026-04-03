d1076c63143b64f6af68358e2a5cbfcdd5336b5e
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-03 07:10:09 +0600

    nameRef add

--- Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
+++ Lesson/Lesson-11/src/Components/SubmitForm/SubmitForm.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const SubmitForm = () => {
+  const nameRef = useRef("");
+
   const handleSubmitForm = (e) => {
     e.preventDefault();
+    console.log(nameRef.current.value);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmitForm}>
-        <input type="text" name="text" />
+        <input ref={nameRef} type="text" name="text" />
         <br />
         <br />
         <input type="email" name="email" />
