a1a5b283e9e7d222c311fd495d7c6cb8d1c96c81
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-03 08:53:19 +0600

    call bug fixed

--- Lesson/Lesson-12/src/Components/Counter2/Counter2.jsx
+++ Lesson/Lesson-12/src/Components/Counter2/Counter2.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import useCounter from "../../Hook/useCounter/useCounter";
 
 const Counter2 = () => {
-  return <div></div>;
+  const [count, increaseHandle, decreaseHandle] = useCounter();
+
+  return (
+    <div>
+      <h1> {count} </h1>
+      <button onClick={increaseHandle}>Increase</button>
+      <br />
+      <br />
+      <button onClick={decreaseHandle}>Decrease</button>
+    </div>
+  );
 };
 
 export default Counter2;
