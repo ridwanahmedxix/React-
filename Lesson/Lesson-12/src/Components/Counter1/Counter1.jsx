d8bd1045182456c76ddf9f7447f977c7f5547330
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-04 07:15:12 +0600

    Counter 1 create

--- Lesson/Lesson-12/src/Components/Counter1/Counter1.jsx
+++ Lesson/Lesson-12/src/Components/Counter1/Counter1.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter1 = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseHandle = () => {
+    setCount(count + 1);
+  };
+  const decreaseHandle = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
-      <button></button>
+      <button onClick={increaseHandle}> Increase </button>
+      <br />
+      <br />
+      <button onClick={decreaseHandle}> Decrease </button>
     </div>
   );
 };
