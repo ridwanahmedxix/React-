405b5bb2dfbe99bf7ab7046552e59cbfb0d3f7f8
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-04 09:51:29 +0600

    useMemo learn

--- Lesson/Lesson-12/src/App.jsx
+++ Lesson/Lesson-12/src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import "./App.css";
-import Counter1 from "./Components/Counter1/Counter1";
-import Counter2 from "./Components/Counter2/Counter2";
 
 function App() {
+  const [increase, setIncrease] = useState(0);
+  const [decrease, setDecrease] = useState(50);
+
+  const problem = useMemo(() => {
+    console.log("Something is loading");
+    return increase * 5;
+  }, [increase]);
+
   return (
     <div>
-      <Counter1></Counter1>
-      <Counter2></Counter2>
+      <h1> {problem} </h1>
+      <br />
+
+      <button onClick={() => setIncrease(increase + 1)}>
+        Increase - {increase}{" "}
+      </button>
+      <br />
+      <button onClick={() => setDecrease(decrease - 1)}>
+        Decrease - {decrease}{" "}
+      </button>
     </div>
   );
 }
-
 export default App;
