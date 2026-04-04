bcbf35b31e5c12d809da5803204eacdad2fd128d
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-04 19:18:03 +0600

    react class - 29 code revision

--- Lesson/Lesson-12/src/App.jsx
+++ Lesson/Lesson-12/src/App.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
   const [increase, setIncrease] = useState(0);
   const [decrease, setDecrease] = useState(60);
 
+  const problem = useMemo(() => {
+    console.log("Something is loading ");
+    return increase / 2;
+  }, [increase]);
+
   return (
     <div>
+      <h1> {problem} </h1>
+      <br />
+
       <button onClick={() => setIncrease(increase + 1)}>
         Increase - {increase}
       </button>
