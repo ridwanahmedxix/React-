db1dbcab3c5a2912535e3dd64e696e3daaebcd2a
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-05 07:17:40 +0600

    loading function create

--- Lesson/Lesson-12/src/App.jsx
+++ Lesson/Lesson-12/src/App.jsx
@@ -4,8 +4,15 @@ import "./App.css";
 function App() {
   const [increase, setIncrease] = useState(0);
   const [decrease, setDecrease] = useState(0);
+
+  const loading = () => {
+    console.log("Something is loading");
+  };
+
   return (
     <div>
+      <h1> {loading()} </h1>
+      <br />
       <button onClick={() => setIncrease(increase + 1)}> Increase </button>
       <button onClick={() => setDecrease(decrease - 1)}> Decrease </button>
     </div>
