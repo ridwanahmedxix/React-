30a45fd2094b17b9627432ad63ef9bd5c36063a5
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-05 19:07:21 +0600

    home page basic stracture create

--- Projects/big-project/p1-navbar/src/components/Routes/Home.jsx
+++ Projects/big-project/p1-navbar/src/components/Routes/Home.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>This is a Home Page </h1>
+      <div>
+        <div>
+          <p>Starting At 999dt</p>
+          <h2>The best notebook collection 2025</h2>
+          <p>Exclusive offer -50% off this week</p>
+          <button>Grab the opportunity Now</button>
+        </div>
+        <div>
+          <img
+            className=" max-w-[600px] w-[100%] "
+            src="/photo/still-life-technology-device-minimal-display.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
