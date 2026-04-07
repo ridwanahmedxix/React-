248f9570d2e85db3aeacacb1785bcee589d9bd1b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-07 09:56:19 +0600

    Footer Section Create

--- Projects/big-project/p1-navbar/src/App.jsx
+++ Projects/big-project/p1-navbar/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 }
