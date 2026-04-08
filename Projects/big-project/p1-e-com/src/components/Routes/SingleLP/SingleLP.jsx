e8c93e9d7ef4c43048188bab80c7f71cf2846a9c
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-08 07:00:56 +0600

    Product design some update

--- Projects/big-project/p1-e-com/src/components/Routes/SingleLP/SingleLP.jsx
+++ Projects/big-project/p1-e-com/src/components/Routes/SingleLP/SingleLP.jsx
@@ -7,7 +7,7 @@ const SingleLP = ({ singleLaptop }) => {
     <div className="text-center p-3 mt-8 sm:mt-12 ">
       <img width="170px" src={image} alt="image" />
       <p className="font-bold"> {name} </p>
-      <p className="font-medium text-red-600"> {price} </p>
+      <p className="font-medium text-red-600"> Price -{price} </p>
       <p> {brand} </p>
       <p> {color} </p>
     </div>
