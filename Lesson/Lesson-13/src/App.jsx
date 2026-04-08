7f4b0433f3889d1184931e85db6e10fb11a2516a
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-08 19:37:05 +0600

    basic setup

---
+++ Lesson/Lesson-13/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button>GG Man</button>
+    </div>
+  );
+}
+
+export default App;
