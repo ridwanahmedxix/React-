e49e2c0258307f005ddcb445605eac9ae733e645
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-10 07:41:21 +0600

    project 2 file create

--- Projects/big-project/p2-care-vue/src/App.jsx
+++ Projects/big-project/p2-care-vue/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>hhhh</h1>
-      <Navbar></Navbar>
+      <h1>Hello World</h1>
     </div>
   );
 }
