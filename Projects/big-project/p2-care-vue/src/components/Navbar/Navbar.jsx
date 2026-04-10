2ab95803894524a5888261ecfbd9c0c570d81e35
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-10 08:03:35 +0600

    navbar structure create

--- Projects/big-project/p2-care-vue/src/components/Navbar/Navbar.jsx
+++ Projects/big-project/p2-care-vue/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { IoSettingsSharp } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div>
-      <nav></nav>
+      <nav>
+        <div>
+          <div>
+            <IoSettingsSharp />
+            <h2> Care Vue </h2>
+          </div>
+        </div>
+        <div>
+          <p> Features</p>
+          <p>Learn</p>
+          <p>Blog</p>
+        </div>
+        <div>
+          <button>Book a Demo</button>
+        </div>
+        <div></div>
+      </nav>
     </div>
   );
 };
