2d3428e1a80207409d3ba85b14e693f455a341d1
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-10 08:50:48 +0600

    all update

--- Projects/big-project/p2-care-vue/src/App.jsx
+++ Projects/big-project/p2-care-vue/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
---
+++ Projects/big-project/p2-care-vue/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      {/* 1 SECTION CODE START  */}
+
+      <section>
+        <div class="min-h-screen bg-gradient-to-br from-[#0D9488] via-[#1E40AF] to-[#1E3A8A]">
+          <h1> Hello </h1>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
