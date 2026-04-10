decfd65bb02aa38d3cc5a351d1a48ba1b6b2e14d
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-10 12:24:05 +0600

    all update

--- Projects/big-project/p2-care-vue/src/components/Home/Home.jsx
+++ Projects/big-project/p2-care-vue/src/components/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { IoStarHalf } from "react-icons/io5";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -7,7 +9,43 @@ const Home = () => {
 
       <section>
         <div class="min-h-screen bg-gradient-to-br from-[#0D9488] via-[#1E40AF] to-[#1E3A8A]">
-          <h1> Hello </h1>
+          <div>
+            <div>
+              <div>
+                <IoStarHalf />
+                <button>AI-Powered Care Management</button>
+              </div>
+
+              <h2>
+                CareVue <br />
+                Intelligent Care
+              </h2>
+              <p>
+                The modern operating system for residential care homes. Replace
+                paper workflows with intelligent digital tools designed for CQC
+                compliance and outstanding care.
+              </p>
+
+              <div>
+                <div>
+                  <button>Book a Demo</button>
+                  <FaArrowRight />
+                </div>
+                <div>
+                  <button>See How It Works</button>
+                  <FaArrowRight />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <img
+                className="max-w-[400px] w-[100%] "
+                src="/public/Images/"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </section>
     </div>
