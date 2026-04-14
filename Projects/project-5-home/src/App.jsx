a2757c0edf3a9acc84cb4a7e3a24948cd3411cd1
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-04-14 10:05:23 +0600

    tailwind css install

---
+++ Projects/project-5-home/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-purple-500">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
