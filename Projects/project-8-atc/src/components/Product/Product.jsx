a20db13e4eedff8109023c07614bbfd8197d7e84
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-11 21:38:48 +0600

    some update code

--- Projects/project-8-atc/src/components/Product/Product.jsx
+++ Projects/project-8-atc/src/components/Product/Product.jsx
@@ -5,11 +5,11 @@ const Product = ({ singleLaptop }) => {
 
   return (
     <div className=" border-2 border-blue-600 m-3 rounded-[20px]  py-3 px-2">
-      {/* <p className="py-1"> Id - {id}</p>
+      <p className="py-1"> Id - {id}</p>
       <p className="py-1"> Name - {name}</p>
       <p className="py-1"> Price - {price}</p>
       <p className="py-1"> Brand - {brand}</p>
-      <p className="py-1"> Color - {color}</p> */}
+      <p className="py-1"> Color - {color}</p>
     </div>
   );
 };
