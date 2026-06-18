4b4368b8faec7e4e76e778febacc0614b4a24caa
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-18 20:13:26 +0600

    useState import

---
+++ Practice/practice14/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Products from "./components/Products/Products";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Worrld</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Practice/practice14/src/components/Products/Products.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+
+const Products = () => {
+  const [count, setCount] = useState([]);
+  useEffect(() => {}, []);
+
+  return <div></div>;
+};
+
+export default Products;
