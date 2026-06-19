19bca673fa8429eaa3cc38a9f8986f78dd114c52
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-19 08:10:07 +0600

    useState import

---
+++ Practice/practice14/src/components/Products/Products.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const Products = () => {
+  const [productStore, setProductStore] = useState([]);
+  return <div>Products</div>;
+};
+
+export default Products;
