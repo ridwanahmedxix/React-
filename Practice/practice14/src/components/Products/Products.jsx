62d95a9f2202a07e614ddd5c9a5d0355e4afe3be
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-19 08:16:16 +0600

    useEffect import

--- Practice/practice14/src/components/Products/Products.jsx
+++ Practice/practice14/src/components/Products/Products.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
   const [productStore, setProductStore] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProductStore(data));
+  }, []);
   return <div>Products</div>;
 };
 
