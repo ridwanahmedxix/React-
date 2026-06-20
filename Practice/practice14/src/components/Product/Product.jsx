0dc8b0d2f2419ee6700ff50cdecccd710eeeb464
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-20 15:37:02 +0600

    products cart create

--- Practice/practice14/src/components/Product/Product.jsx
+++ Practice/practice14/src/components/Product/Product.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const Product = ({ singleProduct }) => {
   const { id, name, price, brand, color } = singleProduct;
-
-  return <div>Product</div>;
+  return (
+    <div>
+      <p> Id - {id} </p>
+      <p> Name - {name} </p>
+      <p> Price - {price} </p>
+      <p> Color - {color} </p>
+      <p> Brand - {brand} </p>
+    </div>
+  );
 };
 
 export default Product;
