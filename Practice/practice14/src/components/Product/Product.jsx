b8e55c0cc08dbbb11e37efe7dc4249ddd530e00b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-21 19:50:53 +0600

    some update

--- Practice/practice14/src/components/Product/Product.jsx
+++ Practice/practice14/src/components/Product/Product.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+// import React from "react";
 
-const Product = ({ singleProduct }) => {
-  const { id, name, price, brand, color } = singleProduct;
-  return (
-    <div>
-      <p> Id - {id} </p>
-      <p> Name - {name} </p>
-      <p> Price - {price} </p>
-      <p> Color - {color} </p>
-      <p> Brand - {brand} </p>
-    </div>
-  );
-};
+// const Product = ({ singleProduct }) => {
+//   const { id, name, price, brand, color } = singleProduct;
+//   return (
+//     <div>
+//       <p> Id - {id} </p>
+//       <p> Name - {name} </p>
+//       <p> Price - {price} </p>
+//       <p> Color - {color} </p>
+//       <p> Brand - {brand} </p>
+//     </div>
+//   );
+// };
 
-export default Product;
+// export default Product;
