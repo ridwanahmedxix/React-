2947b2580ba36c2bad0c6d706d8a6964264f1e17
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-26 16:56:23 +0600

    useState import

--- Practice/practice16/src/App.jsx
+++ Practice/practice16/src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Products from "./components/Products/Products";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const AddToCart = () => {};
+
   return (
     <div>
       <Products></Products>
