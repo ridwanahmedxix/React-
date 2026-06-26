489dc182a4369d8d6b1bb2d1026e2144e6d38861
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-26 12:21:42 +0600

    some update

--- Practice/practice15/src/components/OneData/OneData.jsx
+++ Practice/practice15/src/components/OneData/OneData.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const OneData = ({ OneData }) => {
-  return <div>OneData</div>;
+const OneData = ({ item }) => {
+  const { id, name, price, brand, color } = item;
+
+  return (
+    <div>
+      <p> Id - {id} </p>
+      <p> Name - {name} </p>
+      <p> Price - {price} </p>
+      <p> Brand - {brand} </p>
+      <p> Color - {color} </p>
+    </div>
+  );
 };
 
 export default OneData;
--- Practice/practice15/src/components/PlaceMent/PlaceMent.jsx
+++ Practice/practice15/src/components/PlaceMent/PlaceMent.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import OneData from "../OneData/OneData";
 
 const PlaceMent = ({ cart }) => {
   return (
     <div>
-      {cart.map((OneData) => (
-        <OneData key={OneData.id} OneData={OneData}></OneData>
+      {cart.map((item) => (
+        <OneData key={item.id} OneData={item} />
       ))}
     </div>
   );
