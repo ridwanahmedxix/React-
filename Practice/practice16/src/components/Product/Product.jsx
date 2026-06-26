c14bc83fb38772cd6f46a963950bb662c729fc70
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-26 16:55:05 +0600

    Button create and design

--- Practice/practice16/src/components/Product/Product.jsx
+++ Practice/practice16/src/components/Product/Product.jsx
@@ -4,12 +4,16 @@ const Product = ({ singleProduct }) => {
   const { id, name, price, brand, color } = singleProduct;
 
   return (
-    <div className="border-[2px] border-green-500 py-2 px-4 text-center m-3 ">
+    <div className="border-[2px] rounded border-green-500 py-2 px-4 text-center m-3 ">
       <p> Id - {id} </p>
       <p> Name - {name} </p>
       <p> Price - {price} </p>
       <p> Brand - {brand} </p>
       <p> Color - {color} </p>
+      <br />
+      <button className="bg-green-600 text-white py-1 px-2 mb-2 rounded">
+        Add To Cart
+      </button>
     </div>
   );
 };
