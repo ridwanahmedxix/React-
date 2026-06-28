b33213d929d20798aafb2e8e92df564c4b0efdf5
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-28 15:59:15 +0600

    AddToCart Function Create

--- Practice/practice16/src/App.jsx
+++ Practice/practice16/src/App.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import Products from "./components/Products/Products";
+import PlaceMent from "./components/PlaceMent/PlaceMent";
 function App() {
   const [cart, setCart] = useState([]);
 
+  const AddToCart = () => {
+    console.log("Hello World");
+  };
+
   return (
-    <div>
+    <div className="flex gap-6 ">
       <Products></Products>
+      <PlaceMent></PlaceMent>
     </div>
   );
 }
