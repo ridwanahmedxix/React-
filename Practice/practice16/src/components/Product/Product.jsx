775b671152b5704573f00787afa1a2c4150e6054
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-28 15:39:12 +0600

    some update

--- Practice/practice16/src/components/Product/Product.jsx
+++ Practice/practice16/src/components/Product/Product.jsx
@@ -4,7 +4,7 @@ const Product = ({ singleProduct }) => {
   const { id, name, price, brand, color } = singleProduct;
 
   return (
-    <div>
+    <div className="text-center border-[2px] border-green-500 py-2 px-3 rounded ">
       <p> Id - {id} </p>
       <p> Name - {name} </p>
       <p> Price - {price} </p>
