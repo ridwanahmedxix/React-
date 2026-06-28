ed688b6c95998b1f6fd40b6a0aa3c60eaa6e9507
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-28 15:36:15 +0600

    some update

--- Practice/practice16/src/App.jsx
+++ Practice/practice16/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Products from "./components/Products/Products";
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Products></Products>
     </div>
   );
 }
