ed1d3e8c1561282fb80e52f0ea725cd3a5efe21c
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-06-30 16:17:52 +0600

    useState and useEffect import

---
+++ Practice/practice16/src/components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+const Products = () => {
+  const [productStore, setProductStore] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProductStore(data));
+  }, []);
+
+  return <div>Products</div>;
+};
+
+export default Products;
